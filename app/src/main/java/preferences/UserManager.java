e593477172d6a7d6d48c4c1309d7896f8905d012
weichenglin <chenglin198751@qq.com>
2017-05-24 12:21:30 +0800

    init

---
+++ app/src/main/java/preferences/UserManager.java
@@ -0,0 +1,26 @@
+package preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class UserManager {
+
+	private static SharedPreferences getPreferences(final Context context) {
+		return context.getSharedPreferences("user_manager", Context.MODE_PRIVATE);
+	}
+
+	/** 最后的登录人ID */
+	private static final String lastLoginId = "lastLoginId";
+
+	public static void setLastLoginId(final Context context, String lastLogin) {
+		SharedPreferences prefs = getPreferences(context);
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putString(lastLoginId, lastLogin);
+		editor.commit();
+	}
+
+	public static String getLastLoginId(final Context context) {
+		return getPreferences(context).getString(lastLoginId, "");
+	}
+
+}
