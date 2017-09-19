74a0d261d8df3ad429b8dd07e52e8e48a945c72a
weichenglin <chenglin198751@qq.com>
2017-09-19 19:43:41 +0800

    增加首页tab 切换

--- app/src/main/java/main/MainActivity.java
+++ app/src/main/java/main/MainActivity.java
@@ -1,13 +1,37 @@
 package main;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import base.BaseActivity;
 import cheerly.mybaseproject.R;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    public static final int TAB_FIRST = 0;
+    public static final int TAB_SECOND = 1;
+    public static final int TAB_THIRD = 2;
+    public static final int TAB_FOURTH = 3;
+
+    private static final int[] TAB_BOTTOM_ID_ARRAY = {R.id.tab_first, R.id.tab_second, R.id.tab_third, R.id.tab_fourth};
+    private static final int[] TAB_BOTTOM_ICON_ARRAY = {R.drawable.main_first_icon_selector, R.drawable.main_second_icon_selector,
+            R.drawable.main_third_icon_selector, R.drawable.main_fourth_icon_selector};
+    private static final int[] TAB_BOTTOM_NAME_ARRAY = {R.string.host_first_tab, R.string.host_second_tab,
+            R.string.host_third_tab, R.string.host_fourth_tab};
+    private static final int TAB_SIZE = 4;
+    private Fragment[] mFragArray = new Fragment[TAB_SIZE];
+
+    private int mCurrentTab;
+    private MainFirstFragment mMainFirstFragment;
+    private MainSecondFragment mMainSecondFragment;
+    private MainThirdFragment mMainThirdFragment;
+    private MainFourthFragment mMainFourthFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +39,92 @@ public class MainActivity extends BaseActivity {
         setContentLayout(R.layout.activity_main);
         getTitleHelper().hideTitle();
 
-        Intent intent = new Intent(this, TabLayoutActivity.class);
-        startActivity(intent);
-        finish();
+        showTabFragment(TAB_FIRST);
+        initView();
+    }
+
+    private void initView() {
+        for (int index = 0; index < TAB_BOTTOM_ID_ARRAY.length; index++) {
+            findViewById(TAB_BOTTOM_ID_ARRAY[index]).setOnClickListener(this);
+            ImageView imageView = (ImageView) findViewById(TAB_BOTTOM_ID_ARRAY[index]).findViewById(R.id.image_view);
+            TextView textView = (TextView) findViewById(TAB_BOTTOM_ID_ARRAY[index]).findViewById(R.id.text_view);
+
+            imageView.setImageResource(TAB_BOTTOM_ICON_ARRAY[index]);
+            textView.setText(TAB_BOTTOM_NAME_ARRAY[index]);
+        }
+    }
+
+    /**
+     * 用来让某个tab显示
+     */
+    public void showTab(int currentIndex) {
+        showTabFragment(currentIndex);
+        for (int index = 0; index < TAB_BOTTOM_ID_ARRAY.length; index++) {
+            if (currentIndex == index) {
+                findViewById(TAB_BOTTOM_ID_ARRAY[index]).setSelected(true);
+            } else {
+                findViewById(TAB_BOTTOM_ID_ARRAY[index]).setSelected(false);
+            }
+        }
+    }
+
+    private void showTabFragment(int index) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+
+        if (index == 0) {
+            if (mMainFirstFragment == null) {
+                mMainFirstFragment = new MainFirstFragment();
+                mFragArray[index] = mMainFirstFragment;
+            }
+            addFragment(ft, mMainFirstFragment, index);
+        } else if (index == 1) {
+            if (mMainSecondFragment == null) {
+                mMainSecondFragment = new MainSecondFragment();
+                mFragArray[index] = mMainSecondFragment;
+            }
+            addFragment(ft, mMainSecondFragment, index);
+        } else if (index == 2) {
+            if (mMainThirdFragment == null) {
+                mMainThirdFragment = new MainThirdFragment();
+                mFragArray[index] = mMainThirdFragment;
+            }
+            addFragment(ft, mMainThirdFragment, index);
+        } else if (index == 3) {
+            if (mMainFourthFragment == null) {
+                mMainFourthFragment = new MainFourthFragment();
+                mFragArray[index] = mMainFourthFragment;
+            }
+            addFragment(ft, mMainFourthFragment, index);
+        }
+
+        showFragment(ft, index);
+        ft.commitAllowingStateLoss();
+        mCurrentTab = index;
+    }
+
+    public int getCurrentTabIndex() {
+        return mCurrentTab;
+    }
+
+    private void showFragment(FragmentTransaction ft, final int index) {
+        for (int i = 0; i < mFragArray.length; i++) {
+            if (i == index) {
+                if (mFragArray[i] != null) {
+                    ft.show(mFragArray[i]);
+                }
+            } else {
+                if (mFragArray[i] != null) {
+                    ft.hide(mFragArray[i]);
+                }
+            }
+        }
+    }
+
+    private void addFragment(FragmentTransaction ft, Fragment fragment, int fragmentTag) {
+        if (!fragment.isAdded() && fragment.getTag() == null) {
+            ft.add(R.id.fragment_base_id, fragment, fragmentTag + "");
+        }
     }
 
     @Override
@@ -33,7 +140,23 @@ public class MainActivity extends BaseActivity {
         } else if (myAction.equals("2")) {
             Log.v("tag_2", bundle.getString("key"));
         }
+    }
 
-
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tab_first:
+                showTab(TAB_FIRST);
+                break;
+            case R.id.tab_second:
+                showTab(TAB_SECOND);
+                break;
+            case R.id.tab_third:
+                showTab(TAB_THIRD);
+                break;
+            case R.id.tab_fourth:
+                showTab(TAB_FOURTH);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/main/MainSecondFragment.java
@@ -0,0 +1,31 @@
+package main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import base.BaseFragment;
+import cheerly.mybaseproject.R;
+
+/**
+ * Created by chenglin on 2017-9-14.
+ */
+
+public class MainSecondFragment extends BaseFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_second_frag_layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
