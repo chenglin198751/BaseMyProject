19628af0130c18eeb07e4f4d6909f1ff99ddc369
weichenglin <chenglin198751@qq.com>
2017-09-21 15:06:30 +0800

    优化

--- app/src/main/java/view/WebImageView.java
+++ app/src/main/java/view/WebImageView.java
@@ -2,6 +2,7 @@ package view;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -11,7 +12,16 @@ import com.squareup.picasso.Picasso;
  * Created by chenglin on 2017-7-14.
  */
 
-public class WebImageView extends ImageView {
+public class WebImageView extends AppCompatImageView {
+    //    private static final int CACHE_SIZE = (int) (Runtime.getRuntime().maxMemory() / 1024 / 10); //取可用内存的十分之一，单位KB
+//    private static final LruCache<String, GifDrawable> mLruCache = new LruCache<String, GifDrawable>(CACHE_SIZE) {
+//        protected int sizeOf(String key, GifDrawable gifDrawable) {
+//            //计算单个gifDrawable的内存占用，单位KB
+//            Log.v("tag_2","" + (int) (gifDrawable.getAllocationByteCount() / 1024));
+//            return (int) (gifDrawable.getAllocationByteCount() / 1024);
+//        }
+//    };
+
     public WebImageView(Context context) {
         super(context);
     }
