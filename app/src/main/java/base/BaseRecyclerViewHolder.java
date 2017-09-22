ae89c51192dedba46bd17e2a9977fc819dca092e
weichenglin <chenglin198751@qq.com>
2017-09-22 15:01:06 +0800

    增加Recycler的BaseAdapter

---
+++ app/src/main/java/base/BaseRecyclerViewHolder.java
@@ -0,0 +1,14 @@
+package base;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by chenglin on 2017-9-22.
+ */
+
+public class BaseRecyclerViewHolder extends RecyclerView.ViewHolder {
+    public BaseRecyclerViewHolder(View itemView) {
+        super(itemView);
+    }
+}
