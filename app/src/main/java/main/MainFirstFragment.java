eefee1864d0d742510ca67d78dfe9085a80bd509
weichenglin <chenglin198751@qq.com>
2017-10-27 21:17:23 +0800

    优化

--- app/src/main/java/main/MainFirstFragment.java
+++ app/src/main/java/main/MainFirstFragment.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.squareup.picasso.Picasso;
+
 import base.BaseFragment;
+import base.MyApplication;
 import cheerly.mybaseproject.R;
 import utils.MyUri;
+import utils.MyUtils;
+import view.AutoSizeImageView;
 
 /**
  * Created by chenglin on 2017-9-14.
  */
 
 public class MainFirstFragment extends BaseFragment {
+    public static final String picUrl = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1509119782569&di=1df70625d2ff4e43a03030c5ec480aff&imgtype=0&src=http%3A%2F%2Fwww.th7.cn%2Fd%2Ffile%2Fp%2F2015%2F12%2F31%2Fd08fbed4490f7b6196811abbcc31a209.jpg";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,20 +29,22 @@ public class MainFirstFragment extends BaseFragment {
 
     @Override
     public void onViewCreated(View view) {
-        final Button button = (Button) view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendMyBroadcast("action",new Bundle());
-            }
-        });
+        AutoSizeImageView image_1 = (AutoSizeImageView) view.findViewById(R.id.image_1);
+        AutoSizeImageView image_2 = (AutoSizeImageView) view.findViewById(R.id.image_2);
 
-        view.findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        image_1.setWidth(MyUtils.dip2px(200f));
+        image_1.setCorner(MyUtils.dip2px(4f));
 
-            }
-        });
+        image_2.setHeight(MyUtils.dip2px(200f));
+
+        Picasso.with(MyApplication.getApp())
+                .load(picUrl)
+                .into(image_1);
+
+        Picasso.with(MyApplication.getApp())
+                .load(picUrl)
+                .into(image_2);
+
     }
 
     @Override
