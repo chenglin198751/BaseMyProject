83b02bf93b2480ca6922fdea1a8c74cb5d2a7102
weichenglin <chenglin198751@qq.com>
2018-01-20 15:37:35 +0800

    优化

--- app/src/main/java/httpwork/HttpCallback.java
+++ app/src/main/java/httpwork/HttpCallback.java
@@ -4,7 +4,7 @@ package httpwork;
  * Created by chenglin on 2017-6-27.
  */
 public interface HttpCallback {
-    void onFailure(Exception e);
-
     void onSuccess(String result);
+
+    void onFailure(Exception e);
 }
