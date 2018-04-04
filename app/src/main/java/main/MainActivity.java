59581ef997a855aeae360645f1e72bfb3ca8e892
weichenglin1 <weichenglin1@360.cn>
2018-04-04 17:16:02 +0800

    youhua

--- app/src/main/java/main/MainActivity.java
+++ app/src/main/java/main/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         getWindow().setBackgroundDrawable(null);
         setContentLayout(R.layout.activity_main);
-        getTitleHelper().hideTitle();
+//        getTitleHelper().hideTitle();
         mFragHelper = new ShowFragmentHelper(getSupportFragmentManager(), FRAGMENTS);
 
         initView();
