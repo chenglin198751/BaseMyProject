bccecd89429137b5c412d8a57cd85f338afa31e5
weichenglin1 <weichenglin1@360.cn>
2018-06-05 10:54:11 +0800

    ignore

--- app/src/main/java/main/MainFirstFragment.java
+++ app/src/main/java/main/MainFirstFragment.java
@@ -1,32 +1,22 @@
 package main;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.BitmapRegionDecoder;
 import android.os.Bundle;
-import android.support.v4.graphics.BitmapCompat;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+
+import java.util.UUID;
 
 import base.BaseFragment;
 import cheerly.mybaseproject.R;
-import photo.SelectPhotosActivity;
-import test.TestActivity;
-import test.TestFragment;
-import test.TestPullDownRefreshActivity;
-import test.TestRecyclerViewRefreshActivity;
-import utils.BitmapUtils;
-import utils.Constants;
-import utils.MyUtils;
+import utils.DESUtils;
 
 /**
  * Created by chenglin on 2017-9-14.
  */
 
 public class MainFirstFragment extends BaseFragment {
-    private Button button;
+    String aa;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,10 +26,21 @@ public class MainFirstFragment extends BaseFragment {
 
     @Override
     protected void onViewCreated(Bundle savedInstanceState, View view) {
-        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v("tag_2","width = " + Constants.getScreenWidth());
+                aa = DESUtils.encrypt("今天天气真好，一起去郊游吧", "abcdabcd");
+                Log.v("tag_2", "加密 = " + aa);
+            }
+        });
+
+        view.findViewById(R.id.button_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Log.v("tag_2", "解密 = " + DESUtils.decrypt(aa, "abcdabcd"));
+
+                Log.v("tag_2", "解密 = " + UUID.randomUUID());
+
             }
         });
 
