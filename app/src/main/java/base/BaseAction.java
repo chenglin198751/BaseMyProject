2e575f25d35d3d1e0126f91ce65132467ab404f5
weichenglin1 <weichenglin1@360.cn>
2018-06-05 10:52:40 +0800

    优化

--- app/src/main/java/base/BaseAction.java
+++ app/src/main/java/base/BaseAction.java
@@ -4,11 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 
-public class LibAction {
+public class BaseAction {
     public final static String ACTION_BASE_BROADCAST = "ACTION_BASE_BROADCAST";
 
     public static void sendMyBroadcast(String action, Bundle bundle) {
-        Intent intent = new Intent(LibAction.ACTION_BASE_BROADCAST);
+        Intent intent = new Intent(BaseAction.ACTION_BASE_BROADCAST);
         intent.putExtra("action", action);
         intent.putExtra("bundle", bundle);
         LocalBroadcastManager.getInstance(MyApp.getApp()).sendBroadcast(intent);
