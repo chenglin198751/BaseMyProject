e8bce76d756a2e649a903993829703720145d7bf
weichenglin1 <weichenglin1@360.cn>
2018-07-13 17:54:12 +0800

    优化

---
+++ app/src/main/java/base/MyApp.java
@@ -0,0 +1,12 @@
+package base;
+
+/**
+ * Created by weichenglin  on 2018/7/13
+ */
+public class MyApp extends BaseApp {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
