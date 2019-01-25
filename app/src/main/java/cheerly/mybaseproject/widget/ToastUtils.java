eca02a67459a45914f89bd8cfd2f6d641b007a72
weichenglin1 <weichenglin1@360.cn>
2019-01-25 11:32:38 +0800

    修改包名路径

---
+++ app/src/main/java/cheerly/mybaseproject/widget/ToastUtils.java
@@ -0,0 +1,54 @@
+package cheerly.mybaseproject.widget;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import cheerly.mybaseproject.base.BaseApp;
+import cheerly.mybaseproject.R;
+import cheerly.mybaseproject.utils.BaseUtils;
+
+
+/**
+ * Created by chenglin on 2017-7-24.
+ */
+
+public class ToastUtils {
+    private static Toast mToast;
+
+    public static void show(int resId) {
+        show(BaseApp.getApp().getString(resId));
+    }
+
+    public static void show(final String text) {
+        if (BaseUtils.isUiThread()) {
+            showToast(text);
+        } else {
+            BaseUtils.getHandler().post(new Runnable() {
+                @Override
+                public void run() {
+                    showToast(text);
+                }
+            });
+        }
+    }
+
+    private static void showToast(String text) {
+        TextView tvMessage;
+        if (mToast == null) {
+            mToast = new Toast(BaseApp.getApp());
+
+            View view = View.inflate(BaseApp.getApp(), R.layout.my_toast_layout, null);
+            tvMessage = view.findViewById(R.id.message);
+            tvMessage.setText(text);
+
+            mToast.setView(view);
+            mToast.setDuration(Toast.LENGTH_SHORT);
+        } else {
+            tvMessage = mToast.getView().findViewById(R.id.message);
+            tvMessage.setText(text);
+        }
+        mToast.show();
+    }
+
+}
