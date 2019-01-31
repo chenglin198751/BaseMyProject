5973f186bc74105b0b9497f751a29b4850d7c030
weichenglin1 <weichenglin1@360.cn>
2019-01-31 10:44:49 +0800

    update

--- app/src/main/java/cheerly/mybaseproject/view/pullrefresh/CustomRefreshFooter.java
+++ app/src/main/java/cheerly/mybaseproject/view/pullrefresh/CustomRefreshFooter.java
@@ -1,9 +1,8 @@
-package cheerly.mybaseproject.pullrefresh;
+package cheerly.mybaseproject.view.pullrefresh;
 
 import android.content.Context;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
