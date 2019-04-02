7243feb319488bad86cb581cb8277e8f6129b7a2
weichenglin1 <weichenglin1@360.cn>
2019-04-02 15:13:32 +0800

    picasso 替换为 glide

---
+++ app/src/main/java/cheerly/mybaseproject/MyAppGlideModule.java
@@ -0,0 +1,11 @@
+package cheerly.mybaseproject;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by weichenglin1 on 2018/3/26.
+ */
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {}
