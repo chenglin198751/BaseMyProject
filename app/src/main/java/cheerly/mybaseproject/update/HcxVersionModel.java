17a4d5a5b2e9529496bfa36c0cabf2612cdeeafb
weichenglin1 <weichenglin1@360.cn>
2019-04-22 11:21:12 +0800

    加入注解实现枚举

--- app/src/main/java/cheerly/mybaseproject/update/HcxVersionModel.java
+++ app/src/main/java/cheerly/mybaseproject/update/HcxVersionModel.java
@@ -6,14 +6,11 @@ package cheerly.mybaseproject.update;
  */
 
 public class HcxVersionModel {
-    public static transient final int UPDATE_NORMAL = 0;//正常更新
-    public static transient final int UPDATE_FORCE = 1;//强制更新
-
     public int id; //唯一ID
     public String version; //最新版本号
     public String fromVersion; //开始版本号
     public int source; //系统类型：1-iOS 2-Android 3-其他
-    public int forceUpgrade = -1; //是否强制升级：0-普通升级；1-强制升级
+    public int forceUpgrade = VersionUpdateModel.UPDATE_NORMAL; //是否强制升级：0-普通升级；1-强制升级
     public String title; //升级标题
     public String content; //升级描述
     public String url; //下载链接
