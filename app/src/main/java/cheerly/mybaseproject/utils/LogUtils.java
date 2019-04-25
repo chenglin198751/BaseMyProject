b296040bc694978bde189e7570ed1fa3769feac9
weichenglin1 <weichenglin1@360.cn>
2019-04-25 11:23:56 +0800

    增加读写文件方法

--- app/src/main/java/cheerly/mybaseproject/utils/LogUtils.java
+++ app/src/main/java/cheerly/mybaseproject/utils/LogUtils.java
@@ -4,7 +4,11 @@ import android.os.Environment;
 import android.text.TextUtils;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class LogUtils {
     private static boolean isDebug = false;
@@ -69,5 +73,56 @@ public class LogUtils {
     public static void e(String tag, String msg, Throwable tr) {
         if (isDebug)
             Log.e(tag, msg, tr);
+    }
+
+    public static void writeFile(String filePath, String value) {
+        if (TextUtils.isEmpty(filePath)) {
+            return;
+        }
+        File file = new File(filePath);
+        if (!file.exists()) {
+            return;
+        }
+
+        try {
+            FileWriter fileWriter = new FileWriter(file, true);
+            fileWriter.write(value + "\r\n");
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readFile(String filePath) {
+        if (TextUtils.isEmpty(filePath)) {
+            return null;
+        }
+        File file = new File(filePath);
+        if (!file.exists()) {
+            return null;
+        }
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(file));
+            String readString = "";
+            String currentLine;
+            while ((currentLine = in.readLine()) != null) {
+                currentLine += '\n';
+                readString += currentLine;
+            }
+            return readString;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
