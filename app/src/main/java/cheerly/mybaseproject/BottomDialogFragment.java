59cfaaabefe7235b39e5eef00f81bdfed7a88870
weichenglin1 <weichenglin1@360.cn>
2019-08-27 17:09:37 +0800

    modify BottomDialogFragment

--- app/src/main/java/cheerly/mybaseproject/BottomDialogFragment.java
+++ app/src/main/java/cheerly/mybaseproject/BottomDialogFragment.java
@@ -1,11 +1,9 @@
 package cheerly.mybaseproject;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -16,10 +14,8 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import java.util.ArrayList;
 import java.util.List;
 
-import cheerly.mybaseproject.base.BaseRecyclerViewAdapter;
-import cheerly.mybaseproject.base.BaseRecyclerViewHolder;
+import cheerly.mybaseproject.test.TestRecyclerAdapter;
 import cheerly.mybaseproject.utils.Constants;
-import cheerly.mybaseproject.utils.SmartImageLoader;
 
 
 /**
@@ -29,7 +25,7 @@ import cheerly.mybaseproject.utils.SmartImageLoader;
  */
 public class BottomDialogFragment extends BottomSheetDialogFragment {
     private RecyclerView mRecyclerView;
-    private MyAdapter mAdapter;
+    private TestRecyclerAdapter mAdapter;
 
 
     public static BottomDialogFragment newInstance() {
@@ -54,74 +50,18 @@ public class BottomDialogFragment extends BottomSheetDialogFragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        mAdapter = new MyAdapter(getContext());
+        mAdapter = new TestRecyclerAdapter(getContext());
         mRecyclerView = view.findViewById(R.id.list_view);
         LinearLayoutManager mLayoutManager = new LinearLayoutManager(getContext());
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
-        setData(20, true);
-    }
 
-    private boolean setData(int count, boolean isRefresh) {
         List<String> list = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < 20; i++) {
             list.add("" + (mAdapter.getItemCount() + i));
         }
-
-        if (isRefresh) {
-            mAdapter.setDataList(list);
-        } else {
-            mAdapter.appendDataList(list);
-        }
-
-        if (mAdapter.getItemCount() > 20) {
-            return false;
-        }
-
-        return true;
+        mAdapter.setDataList(list);
     }
 
-
-    public static class MyAdapter extends BaseRecyclerViewAdapter<String> {
-
-        private Context mContext;
-
-        public MyAdapter(Context context) {
-            this.mContext = context;
-        }
-
-
-        @Override
-        public ListHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(mContext).inflate(R.layout.test_item_2, parent, false);
-            return new ListHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(BaseRecyclerViewHolder holder, int position) {
-            if (holder instanceof ListHolder) {
-                ListHolder listHolder = (ListHolder) holder;
-                listHolder.onBind(position);
-            }
-
-        }
-
-        private static class ListHolder extends BaseRecyclerViewHolder {
-            private String url = "http://5b0988e595225.cdn.sohucs.com/images/20170922/c7e95cf930a64a27b616e8c77525645b.jpeg";
-            ImageView imageView;
-
-            public ListHolder(View itemView) {
-                super(itemView);
-                imageView = itemView.findViewById(R.id.image_view);
-
-            }
-
-            @Override
-            public void onBind(int position) {
-                SmartImageLoader.getInstance().load(imageView, url, -1, -1);
-            }
-
-        }
-    }
 
 }
