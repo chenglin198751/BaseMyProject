e588dff4dba252b8a7a72197eb993ff07e77d858
weichenglin1 <weichenglin1@360.cn>
2019-08-28 15:31:02 +0800

    GridView或者参吃不齐的瀑布流，带上header

---
+++ app/src/main/java/cheerly/mybaseproject/test/TestGridViewWithHeaderActivity.java
@@ -0,0 +1,55 @@
+package cheerly.mybaseproject.test;
+
+import android.os.Bundle;
+
+import androidx.core.widget.NestedScrollView;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cheerly.mybaseproject.R;
+import cheerly.mybaseproject.base.BaseActivity;
+
+public class TestGridViewWithHeaderActivity extends BaseActivity {
+    private NestedScrollView nestedScrollView;
+
+    private RecyclerView mRecyclerView;
+    private TestRecyclerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentLayout(R.layout.test_gridview_with_header);
+
+        mRecyclerView = findViewById(R.id.recycler_view);
+        nestedScrollView = findViewById(R.id.nested_scroll_view);
+
+        mAdapter = new TestRecyclerAdapter(getContext());
+        GridLayoutManager mLayoutManager = new GridLayoutManager(getContext(), 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+        setData(20, true);
+    }
+
+    private boolean setData(int count, boolean isRefresh) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            list.add("" + (mAdapter.getItemCount() + i));
+        }
+
+        if (isRefresh) {
+            mAdapter.setDataList(list);
+        } else {
+            mAdapter.appendDataList(list);
+        }
+
+        if (mAdapter.getItemCount() > 20) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
