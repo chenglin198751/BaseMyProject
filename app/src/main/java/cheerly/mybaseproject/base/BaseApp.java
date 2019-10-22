643b8d48a22dcbd78a585419d8ad7d6954f2ab1d
weichenglin1 <weichenglin1@360.cn>
2019-10-22 15:51:09 +0800

    push

--- app/src/main/java/cheerly/mybaseproject/base/BaseApp.java
+++ app/src/main/java/cheerly/mybaseproject/base/BaseApp.java
@@ -3,7 +3,10 @@ package cheerly.mybaseproject.base;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+
 import androidx.annotation.CallSuper;
 import androidx.multidex.MultiDex;
 
@@ -31,6 +34,12 @@ public class BaseApp extends Application {
     public void onCreate() {
         super.onCreate();
         application = this;
+
+        // 置入一个不设防的VmPolicy，用于解决某种情况下file://调用失败的问题
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.setVmPolicy(builder.build());
+        }
 
         //只在应用主进程执行
         if (AppHelper.isAppMainProcess()) {
