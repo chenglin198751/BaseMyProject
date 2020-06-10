decd86107146a5316ac4de1d7ff596fa2c775aa5
weichenglin1 <weichenglin1@360.cn>
2020-06-10 13:08:00 +0800

    gson解析时，null或者""如果用Int 接收，可以变为0

--- app/src/main/java/cheerly/mybaseproject/utils/Constants.java
+++ app/src/main/java/cheerly/mybaseproject/utils/Constants.java
@@ -5,8 +5,13 @@ import android.view.Display;
 import android.view.WindowManager;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import cheerly.mybaseproject.base.BaseApp;
+import cheerly.mybaseproject.httpwork.DoubleDefault0Adapter;
+import cheerly.mybaseproject.httpwork.FloatDefault0Adapter;
+import cheerly.mybaseproject.httpwork.IntegerDefault0Adapter;
+import cheerly.mybaseproject.httpwork.LongDefault0Adapter;
 
 /**
  * 公共常量类
@@ -14,7 +19,18 @@ import cheerly.mybaseproject.base.BaseApp;
 public class Constants {
     private static int screenWidth;
     private static int screenHeight;
-    public final static Gson gson = new Gson();
+
+    public final static Gson gson = new GsonBuilder()
+            .registerTypeAdapter(Integer.class, new IntegerDefault0Adapter())
+            .registerTypeAdapter(int.class, new IntegerDefault0Adapter())
+            .registerTypeAdapter(Double.class, new DoubleDefault0Adapter())
+            .registerTypeAdapter(double.class, new DoubleDefault0Adapter())
+            .registerTypeAdapter(Long.class, new LongDefault0Adapter())
+            .registerTypeAdapter(long.class, new LongDefault0Adapter())
+            .registerTypeAdapter(Float.class, new FloatDefault0Adapter())
+            .registerTypeAdapter(float.class, new FloatDefault0Adapter())
+            .create();
+
     public static final String ACTION_GET_PHOTO_LIST = "ACTION_GET_PHOTO_LIST";
     public static final String KEY_PHOTO_LIST = "KEY_PHOTO_LIST";
 
