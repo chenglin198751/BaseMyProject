c8e402e76ca42d162949e87e1c86dc275ede8ad4
weichenglin1 <weichenglin1@360.cn>
2020-06-12 14:03:35 +0800

    添加自定义的进度条样式

--- app/src/main/java/cheerly/mybaseproject/test/TestActivity.java
+++ app/src/main/java/cheerly/mybaseproject/test/TestActivity.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.List;
@@ -23,7 +26,8 @@ import cheerly.mybaseproject.view.pullrefresh.PullToRefreshView;
 import cheerly.mybaseproject.utils.BaseUtils;
 
 public class TestActivity extends BaseActivity {
-
+    private ProgressBar mProgressBar;
+    private int mProgress = 0;
 
 
     @Override
@@ -31,13 +35,22 @@ public class TestActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentLayout(R.layout.test_activity_layout);
 
+        mProgressBar = findViewById(R.id.progress_bar);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mProgress = mProgress + 5;
+                mProgressBar.setProgress(mProgress);
+            }
+        });
+
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
-
 
 
 }
