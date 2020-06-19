0b8027b26c91b747fb7f341181b5fac617bd3d8b
weichenglin1 <weichenglin1@360.cn>
2020-06-19 15:06:57 +0800

    update

--- app/src/main/java/cheerly/mybaseproject/test/TestRecyclerAdapter.java
+++ app/src/main/java/cheerly/mybaseproject/test/TestRecyclerAdapter.java
@@ -5,10 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import cheerly.mybaseproject.R;
 import cheerly.mybaseproject.base.BaseRecyclerViewAdapter;
 import cheerly.mybaseproject.base.BaseRecyclerViewHolder;
+import cheerly.mybaseproject.utils.BaseUtils;
 import cheerly.mybaseproject.utils.SmartImageLoader;
 
 public class TestRecyclerAdapter extends BaseRecyclerViewAdapter<String> {
@@ -35,19 +37,21 @@ public class TestRecyclerAdapter extends BaseRecyclerViewAdapter<String> {
 
     }
 
-    private static class ListHolder extends BaseRecyclerViewHolder {
+    private class ListHolder extends BaseRecyclerViewHolder {
         private String url = "http://5b0988e595225.cdn.sohucs.com/images/20170922/c7e95cf930a64a27b616e8c77525645b.jpeg";
         ImageView imageView;
+        TextView title;
 
         public ListHolder(View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.image_view);
-
+            title = itemView.findViewById(R.id.title);
         }
 
         @Override
         public void onBind(int position) {
-            SmartImageLoader.getInstance().loadRound(imageView, url, -1, -1, 6);
+            SmartImageLoader.getInstance().loadRound(imageView, url, BaseUtils.dip2px(50f), BaseUtils.dip2px(50f), 15);
+            title.setText("标题 " + getData().get(position));
         }
 
     }
