a41ccd14a6991dd5d2fdfb6e7f6d15ca4367a2f4
weichenglin1 <weichenglin1@360.cn>
2020-06-19 15:45:46 +0800

    增加嵌套滑动的view

--- app/src/main/java/cheerly/mybaseproject/test/TestActivity.java
+++ app/src/main/java/cheerly/mybaseproject/test/TestActivity.java
@@ -1,33 +1,22 @@
 package cheerly.mybaseproject.test;
 
-import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
+import java.util.ArrayList;
 import java.util.List;
 
-import cheerly.mybaseproject.base.BaseActivity;
-import cheerly.mybaseproject.base.BaseRecyclerViewAdapter;
-import cheerly.mybaseproject.base.BaseRecyclerViewHolder;
-import cheerly.mybaseproject.bean.ApkItem;
 import cheerly.mybaseproject.R;
-import cheerly.mybaseproject.view.pullrefresh.PullToRefreshView;
-import cheerly.mybaseproject.utils.BaseUtils;
+import cheerly.mybaseproject.base.BaseActivity;
 
 public class TestActivity extends BaseActivity {
-    private ProgressBar mProgressBar;
-    private int mProgress = 0;
+    private RecyclerView mRecyclerView1;
+    private RecyclerView mRecyclerView3;
+    private TestRecyclerAdapter adapter1;
+    private TestRecyclerAdapter adapter3;
+    private List<String> mLists = new ArrayList<>();
 
 
     @Override
@@ -35,16 +24,24 @@ public class TestActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentLayout(R.layout.test_activity_layout);
 
-        mProgressBar = findViewById(R.id.progress_bar);
+        mRecyclerView1 = findViewById(R.id.recyclerView1);
+        mRecyclerView3 = findViewById(R.id.recyclerView3);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mProgress = mProgress + 5;
-                mProgressBar.setProgress(mProgress);
-            }
-        });
+        adapter1 = new TestRecyclerAdapter(getContext());
+        adapter3 = new TestRecyclerAdapter(getContext());
 
+        for (int i = 0; i < 15; i++) {
+            mLists.add(i + "");
+        }
+
+        adapter1.setDataList(mLists);
+        adapter3.setDataList(mLists);
+
+        mRecyclerView1.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerView3.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mRecyclerView1.setAdapter(adapter1);
+        mRecyclerView3.setAdapter(adapter3);
     }
 
     @Override
