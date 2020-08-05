c12e644c73f4f1ca50a9d3a4a7a295deabd88a0d
weichenglin1 <weichenglin1@360.cn>
2020-08-05 20:16:12 +0800

    update

--- app/src/main/java/cheerly/mybaseproject/base/MainApplication.java
+++ app/src/main/java/cheerly/mybaseproject/base/MainApplication.java
@@ -3,7 +3,7 @@ package cheerly.mybaseproject.base;
 /**
  * Created by weichenglin  on 2018/7/13
  */
-public class MyApp extends BaseApp {
+public class MainApplication extends BaseApp {
 
     @Override
     public void onCreate() {
