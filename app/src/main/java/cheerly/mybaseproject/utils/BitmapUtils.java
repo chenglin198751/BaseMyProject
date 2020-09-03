aae6d2b2fac7225dc876755b7ceefe19da492f97
weichenglin1 <weichenglin1@360.cn>
2020-09-03 18:32:32 +0800

    update

--- app/src/main/java/cheerly/mybaseproject/utils/BitmapUtils.java
+++ app/src/main/java/cheerly/mybaseproject/utils/BitmapUtils.java
@@ -5,10 +5,18 @@ import android.content.Context;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.qmuiteam.qmui.util.QMUIDrawableHelper;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -41,42 +49,88 @@ public class BitmapUtils {
         return size;
     }
 
+    //节省每次创建时产生的开销，但要注意多线程操作synchronized
+    private static final Canvas sCanvas = new Canvas();
+
     /**
-     * 把View 生成bitmap
+     * 从一个view创建Bitmap。
+     * 注意点：绘制之前要清掉 View 的焦点，因为焦点可能会改变一个 View 的 UI 状态。
+     * 来源：https://github.com/tyrantgit/ExplosionField
+     *
+     * @param view  传入一个 View，会获取这个 View 的内容创建 Bitmap。
+     * @param scale 缩放比例，对创建的 Bitmap 进行缩放，数值支持从 0 到 1。
      */
-    public static Bitmap createBitmapByView(View view) {
-        //打开图像缓存
-        view.setDrawingCacheEnabled(true);
-        //必须调用measure和layout方法才能成功保存可视组件的截图到png图像文件
-        //测量View大小
-        if (view.getHeight() <= 0) {
-            view.measure(View.MeasureSpec.makeMeasureSpec(view.getWidth(), View.MeasureSpec.EXACTLY),
-                    View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        } else if (view.getHeight() > 0) {
-            view.measure(View.MeasureSpec.makeMeasureSpec(view.getWidth(), View.MeasureSpec.EXACTLY),
-                    View.MeasureSpec.makeMeasureSpec(view.getHeight(), View.MeasureSpec.EXACTLY));
+    public static Bitmap createBitmapFromView(View view, float scale) {
+        if (view instanceof ImageView) {
+            Drawable drawable = ((ImageView) view).getDrawable();
+            if (drawable != null && drawable instanceof BitmapDrawable) {
+                return ((BitmapDrawable) drawable).getBitmap();
+            }
         }
-        //发送位置和尺寸到View及其所有的子View
-        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
-        Bitmap bitmap = null;
-        try {
-            //获得可视组件的截图
-            bitmap = view.getDrawingCache();
-        } catch (Exception e) {
-            e.printStackTrace();
+        view.clearFocus();
+        Bitmap bitmap = createBitmapSafely((int) (view.getWidth() * scale),
+                (int) (view.getHeight() * scale), Bitmap.Config.ARGB_8888, 1);
+        if (bitmap != null) {
+            synchronized (sCanvas) {
+                Canvas canvas = sCanvas;
+                canvas.setBitmap(bitmap);
+                canvas.save();
+                canvas.drawColor(Color.WHITE); // 防止 View 上面有些区域空白导致最终 Bitmap 上有些区域变黑
+                canvas.scale(scale, scale);
+                view.draw(canvas);
+                canvas.restore();
+                canvas.setBitmap(null);
+            }
         }
         return bitmap;
     }
 
+    public static Bitmap createBitmapFromView(View view) {
+        return createBitmapFromView(view, 1f);
+    }
+
     /**
-     * 从view 得到图片
+     * 从一个view创建Bitmap。把view的区域截掉leftCrop/topCrop/rightCrop/bottomCrop
      */
-    public static Bitmap getBitmapFromView(View view) {
-        view.destroyDrawingCache();
-        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
-        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
-        view.setDrawingCacheEnabled(true);
-        return view.getDrawingCache(true);
+    public static Bitmap createBitmapFromView(View view, int leftCrop, int topCrop, int rightCrop, int bottomCrop) {
+        Bitmap originBitmap = QMUIDrawableHelper.createBitmapFromView(view);
+        if (originBitmap == null) {
+            return null;
+        }
+        Bitmap cutBitmap = createBitmapSafely(view.getWidth() - rightCrop - leftCrop, view.getHeight() - topCrop - bottomCrop, Bitmap.Config.ARGB_8888, 1);
+        if (cutBitmap == null) {
+            return null;
+        }
+        Canvas canvas = new Canvas(cutBitmap);
+        Rect src = new Rect(leftCrop, topCrop, view.getWidth() - rightCrop, view.getHeight() - bottomCrop);
+        Rect dest = new Rect(0, 0, view.getWidth() - rightCrop - leftCrop, view.getHeight() - topCrop - bottomCrop);
+        canvas.drawColor(Color.WHITE); // 防止 View 上面有些区域空白导致最终 Bitmap 上有些区域变黑
+        canvas.drawBitmap(originBitmap, src, dest, null);
+        originBitmap.recycle();
+        return cutBitmap;
+    }
+
+    /**
+     * 安全的创建bitmap。
+     * 如果新建 Bitmap 时产生了 OOM，可以主动进行一次 GC - System.gc()，然后再次尝试创建。
+     *
+     * @param width      Bitmap 宽度。
+     * @param height     Bitmap 高度。
+     * @param config     传入一个 Bitmap.Config。
+     * @param retryCount 创建 Bitmap 时产生 OOM 后，主动重试的次数。
+     * @return 返回创建的 Bitmap。
+     */
+    public static Bitmap createBitmapSafely(int width, int height, Bitmap.Config config, int retryCount) {
+        try {
+            return Bitmap.createBitmap(width, height, config);
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            if (retryCount > 0) {
+                System.gc();
+                return createBitmapSafely(width, height, config, retryCount - 1);
+            }
+            return null;
+        }
     }
 
     public static String getPathByUri(Context context, Uri uri) {
