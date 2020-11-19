7ba532eb21e58017974778a35fc1136298d0602c
weichenglin1 <weichenglin1@360.cn>
2020-11-19 15:11:13 +0800

    upgrade

--- app/src/main/java/cheerly/mybaseproject/listener/OnCompressBitmapListener.java
+++ app/src/main/java/cheerly/mybaseproject/listener/OnCompressBitmapListener.java
@@ -3,10 +3,10 @@ package cheerly.mybaseproject.listener;
 /**
  * Created by chenglin on 2017-5-24.
  */
-public interface OnCompressBitmapListener {
+public interface OnCompressBitmapListener<T> {
     void onPrepare();
 
-    void onSucceed(Object object);
+    void onSucceed(T t);
 
     void onError();
 }
