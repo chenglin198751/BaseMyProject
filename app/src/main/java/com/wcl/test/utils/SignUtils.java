1f9f74cb055221ba21008350ce0108977724b802
weichenglin1 <weichenglin1@360.cn>
2021-10-29 16:38:29 +0800

    upgrade
    Former-commit-id: 6159856336bbd62ee8e26d9281544891a995eb98

---
+++ app/src/main/java/com/wcl/test/utils/SignUtils.java
@@ -0,0 +1,91 @@
+package com.wcl.test.utils;
+
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SignUtils {
+    private static final char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9','a', 'b', 'c', 'd', 'e', 'f'};
+
+    /**
+     * 拼接键值对
+     */
+    private static String buildKeyValue(String key, String value, boolean isEncode) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(key);
+        sb.append("=");
+        if (isEncode) {
+            try {
+                sb.append(URLEncoder.encode(value, "UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                sb.append(value);
+            }
+        } else {
+            sb.append(value);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 对支付参数信息进行签名
+     *
+     * @param map 待签名授权信息
+     */
+    public static String getSign(Map<String, String> map, String appKey) {
+        List<String> keys = new ArrayList<String>(map.keySet());
+        // key排序
+        Collections.sort(keys);
+
+        StringBuilder authInfo = new StringBuilder();
+        for (int i = 0; i < keys.size() - 1; i++) {
+            String key = keys.get(i);
+            String value = map.get(key);
+            authInfo.append(buildKeyValue(key, value, false));
+            authInfo.append("&");
+        }
+
+        String tailKey = keys.get(keys.size() - 1);
+        String tailValue = map.get(tailKey);
+        authInfo.append(buildKeyValue(tailKey, tailValue, false));
+
+        authInfo.append("&app_key=").append(appKey); // 关键，将appKey添加到排序后的订单
+        String orderStr = authInfo.toString();
+        Log.i("signStr", "orderStr: " + orderStr);
+        try {
+            return getMD5(orderStr).toUpperCase();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+
+    private static String getMD5(String plaintext) {
+        try {
+            byte[] btInput = plaintext.getBytes();
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            mdInst.update(btInput);
+            byte[] md = mdInst.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
