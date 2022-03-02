dea2c12f40ba745108fdcd7895e7f114b9bc3f04
weichenglin1 <weichenglin1@360.cn>
2022-03-02 10:18:34 +0800

    upgrade

--- app/src/main/java/com/wcl/test/utils/FastBlurUtil.java
+++ app/src/main/java/com/wcl/test/utils/FastBlurUtil.java
@@ -8,7 +8,7 @@ import com.dasu.blur.DBlur;
 import com.dasu.blur.OnBlurListener;
 
 import com.wcl.test.base.BaseApp;
-import com.wcl.test.listener.OnFinishListener;
+import com.wcl.test.listener.OnFinishedListener;
 
 public class FastBlurUtil {
 
@@ -17,7 +17,7 @@ public class FastBlurUtil {
      * @param scale  对原图的缩放比例，默认是8：比如传8就是把原图缩放八分之一的意思。值越大越模糊，耗时越短。
      * @param radius 高斯模糊的半径，默认是4：高斯模糊计算半径，半径越大越模糊，但通常越耗时
      */
-    public static void doBlur(View view, int scale, final int radius, final OnFinishListener<Bitmap> listener) {
+    public static void doBlur(View view, int scale, final int radius, final OnFinishedListener<Bitmap> listener) {
         DBlur.source(view).mode(BlurConfig.MODE_NATIVE).sampling(scale).radius(radius).build()
                 .doBlur(new OnBlurListener() {
                     @Override
@@ -38,7 +38,7 @@ public class FastBlurUtil {
      * @param scale      对原图的缩放比例，默认是8：比如传8就是把原图缩放八分之一的意思。值越大越模糊，耗时越短。
      * @param radius     高斯模糊的半径，默认是4：高斯模糊计算半径，半径越大越模糊，但通常越耗时
      */
-    public static void doBlur(Bitmap sentBitmap, int scale, final int radius, final OnFinishListener<Bitmap> listener) {
+    public static void doBlur(Bitmap sentBitmap, int scale, final int radius, final OnFinishedListener<Bitmap> listener) {
         DBlur.source(BaseApp.getApp(), sentBitmap).mode(BlurConfig.MODE_NATIVE).sampling(scale).radius(radius).build()
                 .doBlur(new OnBlurListener() {
                     @Override
