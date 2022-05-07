ec67a99e2e97734a0f852f7676023dec25af7c5e
weichenglin1 <weichenglin1@360.cn>
2022-05-07 19:52:15 +0800

    增加执行cmd的工具类
    Former-commit-id: 118b7db9449a5d9e7d40faeca0fbabe9d945e2f3

--- app/src/main/java/com/wcl/test/utils/FastBlurUtil.java
+++ app/src/main/java/com/wcl/test/utils/FastBlurUtil.java
@@ -17,17 +17,17 @@ public class FastBlurUtil {
      * @param scale  对原图的缩放比例，默认是8：比如传8就是把原图缩放八分之一的意思。值越大越模糊，耗时越短。
      * @param radius 高斯模糊的半径，默认是4：高斯模糊计算半径，半径越大越模糊，但通常越耗时
      */
-    public static void doBlur(View view, int scale, final int radius, final OnFinishedListener<Bitmap> listener) {
+    public static void doBlur(View view, int scale, final int radius, final OnFinishedListener<Boolean, Bitmap> listener) {
         DBlur.source(view).mode(BlurConfig.MODE_NATIVE).sampling(scale).radius(radius).build()
                 .doBlur(new OnBlurListener() {
                     @Override
                     public void onBlurSuccess(Bitmap bitmap) {
-                        listener.onFinished(bitmap);
+                        listener.onFinished(true, bitmap);
                     }
 
                     @Override
                     public void onBlurFailed() {
-                        listener.onFinished(null);
+                        listener.onFinished(false, null);
                     }
                 });
     }
@@ -38,17 +38,17 @@ public class FastBlurUtil {
      * @param scale      对原图的缩放比例，默认是8：比如传8就是把原图缩放八分之一的意思。值越大越模糊，耗时越短。
      * @param radius     高斯模糊的半径，默认是4：高斯模糊计算半径，半径越大越模糊，但通常越耗时
      */
-    public static void doBlur(Bitmap sentBitmap, int scale, final int radius, final OnFinishedListener<Bitmap> listener) {
+    public static void doBlur(Bitmap sentBitmap, int scale, final int radius, final OnFinishedListener<Boolean, Bitmap> listener) {
         DBlur.source(BaseApp.getApp(), sentBitmap).mode(BlurConfig.MODE_NATIVE).sampling(scale).radius(radius).build()
                 .doBlur(new OnBlurListener() {
                     @Override
                     public void onBlurSuccess(Bitmap bitmap) {
-                        listener.onFinished(bitmap);
+                        listener.onFinished(true, bitmap);
                     }
 
                     @Override
                     public void onBlurFailed() {
-                        listener.onFinished(null);
+                        listener.onFinished(false, null);
                     }
                 });
     }
