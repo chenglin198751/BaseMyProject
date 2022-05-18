726cdb40555a01eb635afe683edcaf0d21d56c09
weichenglin1 <weichenglin1@360.cn>
2022-05-18 17:04:11 +0800

    upgrade
    Former-commit-id: a7b755d138bbb188358029c6e4d88cc1856c71a9

--- app/src/main/java/com/wcl/test/utils/CmdTask.java
+++ app/src/main/java/com/wcl/test/utils/CmdTask.java
@@ -1,35 +1,27 @@
 package com.wcl.test.utils;
 
-import com.wcl.test.listener.OnFinishedListener;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 
 public class CmdTask {
     private final static String TYPE_INPUT = "input";
     private final static String TYPE_error = "error";
 
-    public static void exe(String command, String workDir, OnFinishedListener<Boolean, String> listener) {
-        CmdTask cmdTask = new CmdTask(command, workDir, listener);
+    public static void exe(String command, String workDir) {
+        CmdTask cmdTask = new CmdTask(command, workDir);
         cmdTask.run();
     }
 
     private String mCommand;
     private String mWorkDir;
-    private OnFinishedListener<Boolean, String> mListener;
+
 
     public CmdTask(String command, String workDir) {
-        this(command, workDir, null);
-    }
-
-    public CmdTask(String command, String workDir, OnFinishedListener<Boolean, String> listener) {
         this.mCommand = replacePath(command);
         this.mWorkDir = replacePath(workDir);
-        this.mListener = listener;
     }
 
     public void run() {
@@ -43,12 +35,15 @@ public class CmdTask {
                 work_dirs = new File(mWorkDir);
             }
             process = Runtime.getRuntime().exec(mCommand, null, work_dirs);
-            // Runtime.exec()创建的子进程公用父进程的流，不同平台上，父进程的stream buffer可能被打满导致子进程阻塞，从而永远无法返回。
-            // 针对这种情况，我们只需要将子进程的stream重定向出来即可。
-            new RedirCmdStreamThread(process, process.getInputStream(), TYPE_INPUT).start();
-            new RedirCmdStreamThread(process, process.getErrorStream(), TYPE_error).start();
-
-            exitVal = process.waitFor();
+            StreamPumper inputPumper = new StreamPumper(process.getInputStream(), TYPE_INPUT);
+            StreamPumper errorPumper = new StreamPumper(process.getErrorStream(), TYPE_error);
+            inputPumper.start();
+            errorPumper.start();
+            process.waitFor();
+            inputPumper.join();
+            errorPumper.join();
+            process.destroy();
+            exitVal = process.exitValue();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
             error = e.toString();
@@ -56,48 +51,51 @@ public class CmdTask {
 
         if (error.length() > 0 || exitVal != 0) {
             error = "cmd = " + mCommand + "; exec failed:" + error;
-            if (mListener != null) {
-                mListener.onFinished(false, error);
-            }
+            System.out.println("command_error:" + error);
             throw new RuntimeException(error);
         }
     }
 
-    class RedirCmdStreamThread extends Thread {
-        InputStream is;
-        Process process;
-        String type;
 
-        RedirCmdStreamThread(Process process, InputStream is, String type) {
-            this.is = is;
-            this.process = process;
-            this.type = type;
+    private static class StreamPumper extends Thread {
+        private BufferedReader din;
+        private String messageLevel;
+        private boolean endOfStream = false;
+        private static final int SLEEP_TIME = 5;
+
+        public StreamPumper(InputStream is, String messageLevel) {
+            this.din = new BufferedReader(new InputStreamReader(is));
+            this.messageLevel = messageLevel;
         }
 
-        public void run() {
-            try {
-                InputStreamReader isr = new InputStreamReader(is, Charset.forName("GBK"));
-                BufferedReader br = new BufferedReader(isr);
+        public void pumpStream() throws IOException {
+            if (!this.endOfStream) {
+                String line = this.din.readLine();
+                if (line != null) {
+                    System.out.println("command_" + messageLevel + ":" + line);
+                } else {
+                    this.endOfStream = true;
+                }
+            }
+        }
 
-                String line = "";
-                String line2 = "";
-                while ((line = br.readLine()) != null) {
-                    if (line != null || line.length() > 0) {
-                        line2 = line2 + line + "\n";
+        @Override
+        public void run() {
+            while (!this.endOfStream) {
+                try {
+                    try {
+                        pumpStream();
+                        sleep(5L);
+                    } catch (IOException e) {
+                        return;
                     }
+                } catch (InterruptedException e2) {
                 }
-                if (mListener != null) {
-                    if (type.equals(TYPE_INPUT)) {
-                        mListener.onFinished(true, line2);
-                    }
-                }
-            } catch (IOException ioe) {
-                if (mListener != null) {
-                    if (type.equals(TYPE_INPUT)) {
-                        mListener.onFinished(false, ioe.toString());
-                    }
-                }
-                ioe.printStackTrace();
+            }
+            try {
+                this.din.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
