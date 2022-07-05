92ad8769a672724d4ac4579d7e6654772dec4d15
weichenglin1 <weichenglin1@360.cn>
2022-07-05 15:17:44 +0800

    增加lifecycle-process

--- app/src/main/java/com/wcl/test/base/BaseApp.java
+++ app/src/main/java/com/wcl/test/base/BaseApp.java
@@ -3,18 +3,19 @@ package com.wcl.test.base;
 import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
-import android.os.Bundle;
 
 import androidx.annotation.CallSuper;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ProcessLifecycleOwner;
 import androidx.multidex.MultiDex;
 
 import com.tencent.smtt.sdk.QbSdk;
-
 import com.wcl.test.helper.AppHelper;
 
 public class BaseApp extends Application {
     private static BaseApp application = null;
-    private Activity mTopAct;
 
     public static BaseApp getApp() {
         return application;
@@ -33,55 +34,22 @@ public class BaseApp extends Application {
         super.onCreate();
         application = this;
 
-//        // 置入一个不设防的VmPolicy，用于解决某种情况下file://调用失败的问题
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
-//            StrictMode.setVmPolicy(builder.build());
-//        }
-
         //只在应用主进程执行
         if (AppHelper.isAppMainProcess()) {
             QbSdk.initX5Environment(getApplicationContext(), null);
         }
 
-
-        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+        //监测app位于前台还是后台
+        ProcessLifecycleOwner.get().getLifecycle().addObserver(new DefaultLifecycleObserver() {
             @Override
-            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+            public void onStop(@NonNull LifecycleOwner owner) {
+                DefaultLifecycleObserver.super.onStop(owner);
             }
 
             @Override
-            public void onActivityDestroyed(Activity activity) {
-            }
-
-            @Override
-            public void onActivityStarted(Activity activity) {
-            }
-
-            @Override
-            public void onActivityResumed(Activity activity) {
-                mTopAct = activity;
-            }
-
-            @Override
-            public void onActivityPaused(Activity activity) {
-            }
-
-            @Override
-            public void onActivityStopped(Activity activity) {
-            }
-
-            @Override
-            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+            public void onStart(@NonNull LifecycleOwner owner) {
+                DefaultLifecycleObserver.super.onStart(owner);
             }
         });
-    }
-
-
-    /**
-     * 得到栈顶Activity(有Activity在另一个进程打开就检测不到了)
-     */
-    public Activity getTopActivity() {
-        return mTopAct;
     }
 }
