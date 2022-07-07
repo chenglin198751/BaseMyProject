f92ee690c9495c56f3210c37410e5fee6a6d3f85
weichenglin1 <weichenglin1@360.cn>
2022-07-07 11:15:24 +0800

    upgrade

--- app/src/main/java/com/wcl/test/base/MainApp.java
+++ app/src/main/java/com/wcl/test/base/MainApp.java
@@ -1,9 +1,16 @@
 package com.wcl.test.base;
 
+import android.content.Context;
+
 /**
  * Created by weichenglin  on 2018/7/13
  */
 public class MainApp extends BaseApp {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
 
     @Override
     public void onCreate() {
