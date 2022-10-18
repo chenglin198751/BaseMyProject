d4a0447e86f9f51c58ca835d5451d5645a0f0304
weichenglin1 <weichenglin1@360.cn>
2022-10-18 13:54:40 +0800

    upgrade
    Former-commit-id: 2fffa4ff7db6984c8b84981ba5d26ad9d3bba63d

--- app/src/main/java/com/wcl/test/helper/AppHelper.java
+++ app/src/main/java/com/wcl/test/helper/AppHelper.java
@@ -1,11 +1,16 @@
 package com.wcl.test.helper;
 
 import android.app.ActivityManager;
+import android.app.Application;
 import android.content.Context;
+import android.os.Build;
 import android.text.TextUtils;
 
-import com.wcl.test.base.BaseApp;
 import com.wcl.test.utils.BaseUtils;
+import com.wcl.test.utils.LogUtils;
+
+import java.lang.reflect.Method;
+import java.util.List;
 
 /**
  * Created by chenglin on 2017-5-24.
@@ -17,42 +22,89 @@ public class AppHelper {
     /**
      * 判断是不是UI主进程，因为有些东西只能在UI主进程初始化
      */
-    public static boolean isAppMainProcess() {
+    public static boolean isAppMainProcess(Context context) {
         try {
-            int pid = android.os.Process.myPid();
-            String process = getAppNameByPID(BaseApp.getApp(), pid);
-            if (TextUtils.isEmpty(process)) {
-                return true;
-            } else if (PROCESS_NAME.equalsIgnoreCase(process)) {
+            if (PROCESS_NAME.equalsIgnoreCase(getCurrentProcessName(context))) {
                 return true;
             } else {
                 return false;
             }
-        } catch (Exception e) {
+        } catch (Throwable e) {
             e.printStackTrace();
             return true;
         }
     }
 
-    /**
-     * 根据Pid得到进程名
-     */
-    public static String getAppNameByPID(Context context, int pid) {
-        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        for (android.app.ActivityManager.RunningAppProcessInfo processInfo : manager.getRunningAppProcesses()) {
-            if (processInfo.pid == pid) {
-                return processInfo.processName;
-            }
+    private static String getCurrentProcessName(Context context) {
+        //1)通过Application的API获取当前进程名
+        String currentProcessName = getCurrentProcessNameByApplication();
+        LogUtils.v("AppHelper", "111 = " + currentProcessName);
+        if (!TextUtils.isEmpty(currentProcessName)) {
+            return currentProcessName;
         }
-        return "";
+
+        //2)通过反射ActivityThread获取当前进程名
+        currentProcessName = getCurrentProcessNameByActivityThread();
+        LogUtils.v("AppHelper", "222 = " + currentProcessName);
+        if (!TextUtils.isEmpty(currentProcessName)) {
+            return currentProcessName;
+        }
+
+        //3)通过ActivityManager获取当前进程名
+        currentProcessName = getCurrentProcessNameByActivityManager(context);
+        LogUtils.v("AppHelper", "333 = " + currentProcessName);
+        return currentProcessName;
     }
 
     /**
-     * 获取当前进程名
+     * 通过Application新的API获取进程名，无需反射，无需IPC，效率最高。
      */
-    public static String getCurProcessName(Context context) {
+    private static String getCurrentProcessNameByApplication() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            return Application.getProcessName();
+        }
+        return null;
+    }
+
+    /**
+     * 通过反射ActivityThread获取进程名，避免了ipc
+     */
+    private static String getCurrentProcessNameByActivityThread() {
+        String processName = null;
+        try {
+            final Method declaredMethod = Class.forName("android.app.ActivityThread", false, Application.class.getClassLoader())
+                    .getDeclaredMethod("currentProcessName", (Class<?>[]) new Class[0]);
+            declaredMethod.setAccessible(true);
+            final Object invoke = declaredMethod.invoke(null, new Object[0]);
+            if (invoke instanceof String) {
+                processName = (String) invoke;
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return processName;
+    }
+
+    /**
+     * 通过ActivityManager 获取进程名，需要IPC通信
+     */
+    private static String getCurrentProcessNameByActivityManager(Context context) {
+        if (context == null) {
+            return null;
+        }
         int pid = android.os.Process.myPid();
-        return getAppNameByPID(context, pid);
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        if (am != null) {
+            List<ActivityManager.RunningAppProcessInfo> runningAppList = am.getRunningAppProcesses();
+            if (runningAppList != null) {
+                for (ActivityManager.RunningAppProcessInfo processInfo : runningAppList) {
+                    if (processInfo.pid == pid) {
+                        return processInfo.processName;
+                    }
+                }
+            }
+        }
+        return null;
     }
 
 }
