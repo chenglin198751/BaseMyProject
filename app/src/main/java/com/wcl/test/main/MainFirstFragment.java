41ff683ba8877538ecdc522ab47b152960287cf4
weichenglin1 <weichenglin1@360.cn>
2023-02-08 15:26:27 +0800

    upgrade
    Former-commit-id: 65af2ade3aaa8c095b421c6d27c956aea2e14e49

--- app/src/main/java/com/wcl/test/main/MainFirstFragment.java
+++ app/src/main/java/com/wcl/test/main/MainFirstFragment.java
@@ -1,39 +1,21 @@
 package com.wcl.test.main;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
+import android.view.ViewGroup;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.MultiTransformation;
-import com.bumptech.glide.load.Transformation;
 import com.wcl.test.R;
 import com.wcl.test.base.BaseFragment;
 import com.wcl.test.databinding.MainFirstFragLayoutBinding;
-import com.wcl.test.httpwork.HttpUtils;
 import com.wcl.test.test.TestRecyclerViewRefreshActivity;
-import com.wcl.test.utils.BaseUtils;
-import com.wcl.test.utils.FileUtils;
-import com.wcl.test.utils.SmartImageLoader;
-import com.wcl.test.widget.BaseWebViewActivity;
-
-import java.io.File;
-import java.io.IOException;
-
-import jp.wasabeef.glide.transformations.BlurTransformation;
-import jp.wasabeef.glide.transformations.CropTransformation;
-import okhttp3.Call;
 
 
 /**
  * Created by chenglin on 2017-9-14.
  */
 public class MainFirstFragment extends BaseFragment {
-
+    private MainFirstFragLayoutBinding mViewBinding;
 
     final String apk_path = "http://qd.shouji.qihucdn.com/media/eb1b2f401965fce6f75198d3a5af1299/6305873c18c53.apk";
 
@@ -42,19 +24,17 @@ public class MainFirstFragment extends BaseFragment {
         super.onCreate(savedInstanceState);
     }
 
-
     @Override
     protected void onViewCreated(Bundle savedInstanceState, View view) {
+        mViewBinding = MainFirstFragLayoutBinding.bind(((ViewGroup) view).getChildAt(0));
 
-        view.findViewById(R.id.button_1).setOnClickListener(new View.OnClickListener() {
+        mViewBinding.viewLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(),TestRecyclerViewRefreshActivity.class);
+                Intent intent = new Intent(getActivity(), TestRecyclerViewRefreshActivity.class);
                 startActivity(intent);
             }
         });
-
-
 
     }
 
