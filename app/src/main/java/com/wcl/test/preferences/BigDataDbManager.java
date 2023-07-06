2e1fae484c9119d42959279dd44f142df32e9e5b
weichenglin1 <weichenglin1@360.cn>
2023-07-06 19:44:38 +0800

    upgrade
    Former-commit-id: 3e0823dafd467d5cab965ba41afa0aff81750546

--- app/src/main/java/com/wcl/test/preferences/BigDataDbManager.java
+++ app/src/main/java/com/wcl/test/preferences/BigDataDbManager.java
@@ -7,6 +7,10 @@ import android.text.TextUtils;
 
 import com.wcl.test.utils.AppLogUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class BigDataDbManager {
     private static final String TAG = "KvDbManager";
@@ -23,14 +27,14 @@ public class BigDataDbManager {
         values.put(CommonSQLite.T_VALUE, value);
 
         // 先查询T_KEY列是否有key值，如果有就update，没有就insert
-        final String sql_query = "select * from " + CommonSQLite.TABLE_NAME + " where " + CommonSQLite.T_KEY + "=?";
+        final String sql_query = "select * from " + CommonSQLite.TABLE_NAME + " where " + CommonSQLite.T_KEY + " = ?";
         Cursor cursor = db.rawQuery(sql_query, new String[]{key});
         long row_id = -1;
 
         if (cursor != null) {
             if (cursor.getCount() > 0) {
                 AppLogUtils.v(TAG, "update");
-                row_id = db.update(CommonSQLite.TABLE_NAME, values, CommonSQLite.T_KEY + "=?", new String[]{key});
+                row_id = db.update(CommonSQLite.TABLE_NAME, values, CommonSQLite.T_KEY + " = ?", new String[]{key});
             } else {
                 AppLogUtils.v(TAG, "insert");
                 row_id = db.insert(CommonSQLite.TABLE_NAME, null, values);
@@ -53,7 +57,7 @@ public class BigDataDbManager {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
         String[] columns = new String[]{CommonSQLite.T_VALUE};
-        String selection = CommonSQLite.T_KEY + "=?";
+        String selection = CommonSQLite.T_KEY + " = ?";
         Cursor cursor = db.query(CommonSQLite.TABLE_NAME, columns, selection, new String[]{key}, null, null, null);
 
         if (cursor != null) {
@@ -69,6 +73,63 @@ public class BigDataDbManager {
         return value;
     }
 
+    public static boolean putValues(List<String> keys, List<String> values) {
+        if (keys == null || keys.size() == 0) {
+            return false;
+        } else if (values == null || values.size() == 0) {
+            return false;
+        } else if (keys.size() != values.size()) {
+            throw new IllegalArgumentException("keys.size() != values.size()");
+        }
+
+        CommonSQLite dbHelper = new CommonSQLite();
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        int count = 0;
+        for (int i = 0; i < keys.size(); i++) {
+            ContentValues contents = new ContentValues();
+            contents.put(CommonSQLite.T_KEY, keys.get(i));
+            contents.put(CommonSQLite.T_VALUE, values.get(i));
+            long row_id = db.replace(CommonSQLite.TABLE_NAME, null, contents);
+            if (row_id > 0) {
+                count++;
+            }
+        }
+
+
+        db.close();
+        dbHelper.close();
+        return count == keys.size();
+    }
+
+    public static List<String> getValues(List<String> keys) {
+        if (keys == null || keys.size() == 0) {
+            return null;
+        }
+
+        List<String> values = new ArrayList<>();
+        CommonSQLite dbHelper = new CommonSQLite();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String[] selectionArgs = new String[keys.size()];
+        keys.toArray(selectionArgs);
+
+        String selection = TextUtils.join(",", Collections.nCopies(keys.size(), "?"));
+        String sql_query = "SELECT " + CommonSQLite.T_VALUE + " FROM " + CommonSQLite.TABLE_NAME +
+                " WHERE " + CommonSQLite.T_KEY + " in (" + selection + ")";
+
+        Cursor cursor = db.rawQuery(sql_query, selectionArgs);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                values.add(cursor.getString(0));
+            }
+            cursor.close();
+        }
+
+        db.close();
+        dbHelper.close();
+        return values;
+    }
 
     public static boolean remove(String key) {
         if (TextUtils.isEmpty(key)) {
@@ -78,7 +139,7 @@ public class BigDataDbManager {
         CommonSQLite dbHelper = new CommonSQLite();
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        String selection = CommonSQLite.T_KEY + "=?";
+        String selection = CommonSQLite.T_KEY + " = ?";
         int deletedRows = db.delete(CommonSQLite.TABLE_NAME, selection, new String[]{key});
 
         db.close();
@@ -86,5 +147,26 @@ public class BigDataDbManager {
         return deletedRows > 0;
     }
 
+    public static boolean remove(List<String> keys) {
+        if (keys == null || keys.size() == 0) {
+            return false;
+        }
+
+        CommonSQLite dbHelper = new CommonSQLite();
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        int count = 0;
+        for (int i = 0; i < keys.size(); i++) {
+            String selection = CommonSQLite.T_KEY + " = ?";
+            int deletedRows = db.delete(CommonSQLite.TABLE_NAME, selection, new String[]{keys.get(i)});
+            if (deletedRows > 0) {
+                count++;
+            }
+        }
+
+        db.close();
+        dbHelper.close();
+        return count == keys.size();
+    }
 
 }
