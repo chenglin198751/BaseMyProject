2ceb28a54e597ceb4218c653f7a0e4681f006d00
weichenglin1 <weichenglin1@360.cn>
2024-02-06 15:44:40 +0800

    upgrade
    Former-commit-id: 1add29b64eaa9c5120169509135d6a7b6292186a

--- test_jar_lib/src/main/java/main/CmdTask.java
+++ test_jar_lib/src/main/java/main/CmdTask.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -108,7 +109,7 @@ public class CmdTask {
             InputStreamReader isr = null;
             BufferedReader br = null;
             try {
-                isr = new InputStreamReader(is);
+                isr = new InputStreamReader(is, StandardCharsets.UTF_8);
                 br = new BufferedReader(isr);
 
                 String line = "";
