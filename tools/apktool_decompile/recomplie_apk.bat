a142e2d39585031e9b9b3cc9bca2326fe7b2d8e7
weichenglin1 <weichenglin1@360.cn>
2025-05-13 13:58:23 +0800

    upgrade
    Former-commit-id: 78a263cf67b17ec6a05a8895b521c519ebb5386e

--- tools/apktool_decompile/recomplie_apk.bat
+++ tools/apktool_decompile/recomplie_apk.bat
@@ -8,11 +8,14 @@ java -jar tools/apktool.jar -v b -f %1 -o unsign.apk
 
 @echo --------------start sign apk....--------------
 
-java -jar tools/apksigner.jar sign --v1-signing-enabled true --v2-signing-enabled true --v3-signing-enabled false --v4-signing-enabled false --ks tools/keystore_debug.jks --ks-key-alias young_debug --ks-pass pass:123abc --key-pass pass:123abc --out signed_output.apk unsign.apk
+tools\zipalign.exe -f 4 unsign.apk zipalign.apk
+
+java -jar tools\apksigner.jar sign --v1-signing-enabled true --v2-signing-enabled true --v3-signing-enabled true --v4-signing-enabled false --ks tools\keystore_debug.jks --ks-key-alias young_debug --ks-pass pass:123abc --key-pass pass:123abc --out signed_output.apk zipalign.apk
 
 @echo --------------compile apk successful--------------
 @echo --------------------------------------------------------
 
 del unsign.apk
+del zipalign.apk
 
 pause
