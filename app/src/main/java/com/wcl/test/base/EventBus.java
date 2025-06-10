112a308b11772a8f1a504cf50c62bc1ddbb933d2
weichenglin1 <weichenglin1@360.cn>
2025-06-10 15:43:21 +0800

    upgrade
    Former-commit-id: 878f6f06f2f4168fa675fccd7382253c4a7468bf

--- app/src/main/java/com/wcl/test/base/EventBus.java
+++ app/src/main/java/com/wcl/test/base/EventBus.java
@@ -25,15 +25,18 @@ public class EventBus {
         /**
          * 系统基础广播
          */
-        String ACTION_BASE_BROADCAST = "ACTION_BASE_BROADCAST";
+        String ACTION_BASE_BROADCAST = "ACTION_SYS_BASE_BROADCAST";
         /**
          * 根据开关onKeepSingleActivity()：当前Activity无论打开多少，只保留最后打开的一个
          */
-        String ACTION_KEEP_SINGLE_ACTIVITY = "ACTION_KEEP_SINGLE_ACTIVITY";
+        String ACTION_KEEP_SINGLE_ACTIVITY = "ACTION_SYS_KEEP_SINGLE_ACTIVITY";
         /**
          * 关闭别的Activity，只保留MainActivity不关闭
          */
-        String ACTION_KEEP_MAIN_AND_CLOSE_ACTIVITY = "ACTION_KEEP_MAIN_AND_CLOSE_ACTIVITY";
+        String ACTION_KEEP_MAIN_AND_CLOSE_ACTIVITY = "ACTION_SYS_KEEP_MAIN_AND_CLOSE_ACTIVITY";
     }
 
+    public interface App {
+        String action_test = "ACTION_TEST";
+    }
 }
