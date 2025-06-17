099163176cdbf1e44456eaaefb115fcc86248beb
weichenglin1 <weichenglin1@360.cn>
2025-06-17 17:32:35 +0800

    upgrade
    Former-commit-id: e0bf92c7eb35b5c5b6736082082537dd984aaabd

--- app/src/main/java/com/wcl/test/utils/FastBlurUtil2.java
+++ app/src/main/java/com/wcl/test/utils/FastBlurUtil2.java
@@ -2,134 +2,85 @@ package com.wcl.test.utils;
 
 import android.graphics.Bitmap;
 import android.view.View;
-
 import com.wcl.test.listener.OnFinishedListener;
 
-
 /**
- * Stack Blur v1.0 from
- * http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
- * <p>
+ * java 原生api 实现的
+ * Stack Blur v1.0 from http://www.quasimondo.com/StackBlurForCanvas/StackBlurDemo.html
  * Java Author: Mario Klingemann <mario at quasimondo.com>
- * http://incubator.quasimondo.com
- * created Feburary 29, 2004
  * Android port : Yahel Bouaziz <yahel at kayenko.com>
- * http://www.kayenko.com
- * ported april 5th, 2012
- * <p>
- * This is a compromise between Gaussian Blur and Box blur
- * It creates much better looking blurs than Box Blur, but is
- * 7x faster than my Gaussian Blur implementation.
- * <p>
- * I called it Stack Blur because this describes best how this
- * filter works internally: it creates a kind of moving stack
- * of colors whilst scanning through the image. Thereby it
- * just has to add one new block of color to the right side
- * of the stack and remove the leftmost color. The remaining
- * colors on the topmost layer of the stack are either added on
- * or reduced by one, depending on if they are on the right or
- * on the left side of the stack.
- * <p>
- * If you are using this algorithm in your code please add
- * the following line:
  * <p>
  * Stack Blur Algorithm by Mario Klingemann <mario@quasimondo.com>
  */
-
-//java 原生api 实现的
 public class FastBlurUtil2 {
 
     /**
+     * 对View进行高斯模糊，异步返回Bitmap
      * @param view   要被高斯模糊的View 生成的图片
-     * @param scale  对原图的缩放比例：从0.1到1取值，值越小速度越快，值越大速度越慢。大图建议值0.1~0.2，小图建议值0.2~0.4
-     * @param radius 高斯模糊的程度：从1到100取值。值越小越清晰(速度快)，越大越模糊(速度慢)，建议值10~20
-     * @return
+     * @param scale  对原图的缩放比例：0.1~1，建议大图0.1~0.2，小图0.2~0.4
+     * @param radius 高斯模糊程度：1~100，建议10~20
+     * @param listener 结果回调
      */
-    public static <OnFinishListener> void doBlur(final View view, final float scale, final int radius,
-                                                 final OnFinishedListener<Boolean, Bitmap> listener) {
+    public static void doBlur(final View view, final float scale, final int radius,
+                              final OnFinishedListener<Boolean, Bitmap> listener) {
         final Bitmap sentBitmap = BitmapUtils.createBitmapFromView(view);
-        new Thread() {
-            @Override
-            public void run() {
-                super.run();
-                final Bitmap bitmap = doBlur(sentBitmap, scale, radius);
-                AppBaseUtils.getUiHandler().post(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (listener != null) {
-                            listener.onFinished(true, bitmap);
-                        }
-                    }
-                });
-            }
-        }.start();
+        new Thread(() -> {
+            Bitmap bitmap = doBlur(sentBitmap, scale, radius);
+            AppBaseUtils.getUiHandler().post(() -> {
+                if (listener != null) {
+                    listener.onFinished(true, bitmap);
+                }
+            });
+        }).start();
     }
 
-
+    /**
+     * 对Bitmap执行高斯模糊
+     * @param sentBitmap 原始Bitmap
+     * @param scale      缩放比例
+     * @param radius     模糊半径
+     * @return 模糊后Bitmap
+     */
     public static Bitmap doBlur(Bitmap sentBitmap, float scale, final int radius) {
-//        final boolean canReuseInBitmap = false;
-        if (scale <= 0 || scale > 1) {
-            scale = 1;
-        }
+        if (sentBitmap == null) return null;
+        if (scale <= 0f || scale > 1f) scale = 1f;
+        if (radius < 1) return sentBitmap;
 
-        float scaledWidth = sentBitmap.getWidth() * scale;
-        float scaledHeight = sentBitmap.getHeight() * scaledWidth / sentBitmap.getWidth();
-        Bitmap bitmap = Bitmap.createScaledBitmap(sentBitmap, (int) scaledWidth, (int) scaledHeight, true);
-
-//        Bitmap bitmap;
-//        if (canReuseInBitmap) {
-//            bitmap = sentBitmap;
-//        } else {
-//            bitmap = sentBitmap.copy(sentBitmap.getConfig(), true);
-//        }
-
-        if (radius < 1) {
-            return (null);
-        }
+        int srcWidth = sentBitmap.getWidth();
+        int srcHeight = sentBitmap.getHeight();
+        int scaledWidth = Math.max(1, Math.round(srcWidth * scale));
+        int scaledHeight = Math.max(1, Math.round(srcHeight * scale));
+        Bitmap bitmap = Bitmap.createScaledBitmap(sentBitmap, scaledWidth, scaledHeight, true);
 
         int w = bitmap.getWidth();
         int h = bitmap.getHeight();
-
         int[] pix = new int[w * h];
         bitmap.getPixels(pix, 0, w, 0, 0, w, h);
 
-        int wm = w - 1;
-        int hm = h - 1;
-        int wh = w * h;
-        int div = radius + radius + 1;
-
-        int r[] = new int[wh];
-        int g[] = new int[wh];
-        int b[] = new int[wh];
+        int wm = w - 1, hm = h - 1, wh = w * h, div = radius + radius + 1;
+        int[] r = new int[wh], g = new int[wh], b = new int[wh];
         int rsum, gsum, bsum, x, y, i, p, yp, yi, yw;
-        int vmin[] = new int[Math.max(w, h)];
-
+        int[] vmin = new int[Math.max(w, h)];
         int divsum = (div + 1) >> 1;
         divsum *= divsum;
-        int dv[] = new int[256 * divsum];
-        for (i = 0; i < 256 * divsum; i++) {
-            dv[i] = (i / divsum);
-        }
-
-        yw = yi = 0;
+        int[] dv = new int[256 * divsum];
+        for (i = 0; i < dv.length; i++) dv[i] = (i / divsum);
 
         int[][] stack = new int[div][3];
-        int stackpointer;
-        int stackstart;
+        int stackpointer, stackstart;
         int[] sir;
-        int rbs;
-        int r1 = radius + 1;
-        int routsum, goutsum, boutsum;
-        int rinsum, ginsum, binsum;
+        int rbs, r1 = radius + 1;
+        int routsum, goutsum, boutsum, rinsum, ginsum, binsum;
 
+        yw = yi = 0;
         for (y = 0; y < h; y++) {
             rinsum = ginsum = binsum = routsum = goutsum = boutsum = rsum = gsum = bsum = 0;
             for (i = -radius; i <= radius; i++) {
                 p = pix[yi + Math.min(wm, Math.max(i, 0))];
                 sir = stack[i + radius];
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
+                sir[0] = (p >> 16) & 0xff;
+                sir[1] = (p >> 8) & 0xff;
+                sir[2] = p & 0xff;
                 rbs = r1 - Math.abs(i);
                 rsum += sir[0] * rbs;
                 gsum += sir[1] * rbs;
@@ -145,9 +96,7 @@ public class FastBlurUtil2 {
                 }
             }
             stackpointer = radius;
-
             for (x = 0; x < w; x++) {
-
                 r[yi] = dv[rsum];
                 g[yi] = dv[gsum];
                 b[yi] = dv[bsum];
@@ -156,21 +105,19 @@ public class FastBlurUtil2 {
                 gsum -= goutsum;
                 bsum -= boutsum;
 
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
+                stackstart = (stackpointer - radius + div) % div;
+                sir = stack[stackstart];
 
                 routsum -= sir[0];
                 goutsum -= sir[1];
                 boutsum -= sir[2];
 
-                if (y == 0) {
-                    vmin[x] = Math.min(x + radius + 1, wm);
-                }
+                if (y == 0) vmin[x] = Math.min(x + r1, wm);
                 p = pix[yw + vmin[x]];
 
-                sir[0] = (p & 0xff0000) >> 16;
-                sir[1] = (p & 0x00ff00) >> 8;
-                sir[2] = (p & 0x0000ff);
+                sir[0] = (p >> 16) & 0xff;
+                sir[1] = (p >> 8) & 0xff;
+                sir[2] = p & 0xff;
 
                 rinsum += sir[0];
                 ginsum += sir[1];
@@ -181,7 +128,7 @@ public class FastBlurUtil2 {
                 bsum += binsum;
 
                 stackpointer = (stackpointer + 1) % div;
-                sir = stack[(stackpointer) % div];
+                sir = stack[stackpointer];
 
                 routsum += sir[0];
                 goutsum += sir[1];
@@ -200,19 +147,14 @@ public class FastBlurUtil2 {
             yp = -radius * w;
             for (i = -radius; i <= radius; i++) {
                 yi = Math.max(0, yp) + x;
-
                 sir = stack[i + radius];
-
                 sir[0] = r[yi];
                 sir[1] = g[yi];
                 sir[2] = b[yi];
-
                 rbs = r1 - Math.abs(i);
-
                 rsum += r[yi] * rbs;
                 gsum += g[yi] * rbs;
                 bsum += b[yi] * rbs;
-
                 if (i > 0) {
                     rinsum += sir[0];
                     ginsum += sir[1];
@@ -222,31 +164,24 @@ public class FastBlurUtil2 {
                     goutsum += sir[1];
                     boutsum += sir[2];
                 }
-
-                if (i < hm) {
-                    yp += w;
-                }
+                if (i < hm) yp += w;
             }
             yi = x;
             stackpointer = radius;
             for (y = 0; y < h; y++) {
-                // Preserve alpha channel: ( 0xff000000 & pix[yi] )
                 pix[yi] = (0xff000000 & pix[yi]) | (dv[rsum] << 16) | (dv[gsum] << 8) | dv[bsum];
-
                 rsum -= routsum;
                 gsum -= goutsum;
                 bsum -= boutsum;
 
-                stackstart = stackpointer - radius + div;
-                sir = stack[stackstart % div];
+                stackstart = (stackpointer - radius + div) % div;
+                sir = stack[stackstart];
 
                 routsum -= sir[0];
                 goutsum -= sir[1];
                 boutsum -= sir[2];
 
-                if (x == 0) {
-                    vmin[y] = Math.min(y + r1, hm) * w;
-                }
+                if (x == 0) vmin[y] = Math.min(y + r1, hm) * w;
                 p = x + vmin[y];
 
                 sir[0] = r[p];
@@ -275,10 +210,7 @@ public class FastBlurUtil2 {
                 yi += w;
             }
         }
-
         bitmap.setPixels(pix, 0, w, 0, 0, w, h);
-
-        return (bitmap);
+        return bitmap;
     }
-
 }
