8d8828a1027d619d2f1fb24ec1ef57a589266850
weichenglin1 <weichenglin1@360.cn>
2025-06-17 15:30:48 +0800

    upgrade
    Former-commit-id: 2ccf7ecbcf0d55a28e93379fefd66753268f00ca

--- app/src/main/java/com/wcl/test/utils/PopupWindowUtils.java
+++ app/src/main/java/com/wcl/test/utils/PopupWindowUtils.java
@@ -3,13 +3,13 @@ package com.wcl.test.utils;
 import android.app.Activity;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.PopupWindow;
 
 import com.wcl.test.R;
-
 
 /**
  * weichenglin create in 15/5/12
@@ -22,60 +22,65 @@ public class PopupWindowUtils {
      * popView.setTag(R.id.offset_x,px) 和 popView.setTag(R.id.offset_y,px) 可以设置当前显示popWindow的坐标偏移量
      */
     public static PopupWindow show(Activity activity, View attachOnView, View popView, final int popShowHeight, final int popShowWidth) {
-        if (popView != null && popView.getParent() != null) {
+        if (activity == null || attachOnView == null) {
+            return null;
+        }
+
+        if (popView != null && popView.getParent() instanceof ViewGroup) {
             ((ViewGroup) popView.getParent()).removeAllViews();
         }
+
         if (popView == null) {
             return null;
         }
 
-        PopupWindow popupWindow = null;
         Rect frame = new Rect();
         activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
 
-        int location[] = new int[2];
-        int x, y;
-        int popHeight = 0, popWidth = 0;
-
+        int[] location = new int[2];
         attachOnView.getLocationInWindow(location);
-        x = location[0];
-        y = location[1];
+        int x = location[0];
+        int y = location[1];
 
         Integer offsetX = (Integer) popView.getTag(R.id.offset_x);
         Integer offsetY = (Integer) popView.getTag(R.id.offset_y);
 
         if (offsetX != null) {
-            x = x + offsetX;
+            x += offsetX;
         }
         if (offsetY != null) {
-            y = y + offsetY;
+            y += offsetY;
         }
 
         int h = attachOnView.getHeight();
-        int screenHeight = AppConstants.screenWidth;
 
+        DisplayMetrics metrics = activity.getResources().getDisplayMetrics();
+        int screenHeight = metrics.heightPixels;
+
+        int popHeight;
         if (popShowHeight == defaultBotom) {
-            popHeight = screenHeight / 6;
-            popHeight = Math.abs(screenHeight - (h + y)) - popHeight;
-        } else if (popHeight == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            // 默认 bottom 高度为屏幕剩余空间减去 1/6 屏幕高度
+            popHeight = Math.abs(screenHeight - (h + y)) - screenHeight / 6;
+        } else if (popShowHeight == ViewGroup.LayoutParams.WRAP_CONTENT) {
             popHeight = ViewGroup.LayoutParams.WRAP_CONTENT;
         } else {
             popHeight = popShowHeight;
         }
 
+        int popWidth;
         if (popShowWidth == ViewGroup.LayoutParams.WRAP_CONTENT) {
             popWidth = attachOnView.getWidth();
         } else {
             popWidth = popShowWidth;
         }
 
-        popupWindow = new PopupWindow(popView, popWidth, popHeight, true);
+        PopupWindow popupWindow = new PopupWindow(popView, popWidth, popHeight, true);
         popupWindow.setBackgroundDrawable(new BitmapDrawable());
         popupWindow.setOutsideTouchable(true);
-
         popupWindow.setAnimationStyle(R.style.PopupAnimationDown);
         popupWindow.showAtLocation(attachOnView, Gravity.NO_GRAVITY, x, h + y);
         popupWindow.update();
+
         return popupWindow;
     }
 }
