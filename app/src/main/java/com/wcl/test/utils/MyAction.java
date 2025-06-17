97c1db444888ff633fc8765ad3c53253feb6a0c6
weichenglin1 <weichenglin1@360.cn>
2025-06-17 15:24:08 +0800

    upgrade
    Former-commit-id: 12b411724ed6c8ea2e38d231730d6f661c540d3c

--- app/src/main/java/com/wcl/test/utils/MyAction.java
+++
@@ -1,5 +0,0 @@
-package com.wcl.test.utils;
-
-public class MyAction {
-
-}
--- app/src/main/java/com/wcl/test/utils/ReflectUtils.java
+++ app/src/main/java/com/wcl/test/utils/ReflectUtils.java
@@ -2,332 +2,143 @@ package com.wcl.test.utils;
 
 import android.content.Context;
 import android.os.IBinder;
-import android.os.Looper;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class ReflectUtils {
-    public ReflectUtils() {
+    private static final String TAG = "ReflectUtils";
+
+    private ReflectUtils() {
+        // 私有构造函数防止实例化
     }
-
-    public static Method getDeclaredMethod(Object object, String methodName, Class<?>... parameterTypes) {
-        Class clazz = object instanceof Class ? (Class) object : object.getClass();
-
-        while (clazz != Object.class) {
-            try {
-                return clazz.getDeclaredMethod(methodName, parameterTypes);
-            } catch (Exception var5) {
-                clazz = clazz.getSuperclass();
-            }
-        }
-
-        throw new RuntimeException("getDeclaredMethod exception, object = " + object + ", methodName = " + methodName);
-    }
-
 
     /**
-     * 反射一个类的调用方法
-     *
-     * @param clazz          类（如baseUtils.getClass()）
-     * @param methodName     原方法的方法名
-     * @param parameterTypes 原方法参数的数据类型，数组类型的类名（如{String.class}）
-     * @param parameters     原方法的参数值，数据类型
-     * @return object原方法返回值
-     * 用法如下：
-     * Class<?>[] parameterTypes = {String.class};
-     * Object[] parameters = {"version name value"};
-     * BaseUtils baseUtils = new BaseUtils();
-     * baseUtils.getClass();
-     * ReflectUtils.invokeMethod(baseUtils.getClass(),"setTestMethod",parameterTypes,parameters);
+     * 获取指定类的DeclaredMethod（支持继承链查找）
      */
-    public static Object invokeMethod(Class clazz, String methodName, Class<?>[] parameterTypes, Object... parameters) {
-        try {
-            Method method = clazz.getDeclaredMethod(methodName, parameterTypes);
-            if (parameters != null && parameters.length > 0) {
-                return method.invoke(clazz.newInstance(), parameters);
-            } else {
-                return method.invoke(clazz.newInstance());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static Field getDeclaredField(Object object, String fieldName) {
-        Class clazz = object.getClass();
+    public static Method getDeclaredMethod(Object object, String methodName, Class<?>... parameterTypes) throws NoSuchMethodException {
+        Class<?> clazz = (object instanceof Class<?>) ? (Class<?>) object : object.getClass();
 
         while (clazz != Object.class) {
             try {
-                return clazz.getDeclaredField(fieldName);
-            } catch (Exception var4) {
+                Method method = clazz.getDeclaredMethod(methodName, parameterTypes);
+                method.setAccessible(true);
+                return method;
+            } catch (NoSuchMethodException e) {
                 clazz = clazz.getSuperclass();
             }
         }
 
-        throw new RuntimeException("getDeclaredField exception, object = " + object + ", fieldName = " + fieldName);
+        throw new NoSuchMethodException("Method " + methodName + " not found in " + object.getClass());
+    }
+
+    /**
+     * 调用指定类的静态方法
+     */
+    public static Object invokeStaticMethod(Class<?> clazz, String methodName, Class<?>[] parameterTypes, Object... parameters) throws Exception {
+        Method method = getDeclaredMethod(clazz, methodName, parameterTypes);
+        return method.invoke(null, parameters);
+    }
+
+    /**
+     * 调用对象的方法
+     */
+    public static Object invokeMethod(Object object, String methodName, Class<?>[] parameterTypes, Object... parameters) throws Exception {
+        Method method = getDeclaredMethod(object, methodName, parameterTypes);
+        return method.invoke(object, parameters);
+    }
+
+    /**
+     * 获取指定类的DeclaredField（支持继承链查找）
+     */
+    public static Field getDeclaredField(Object object, String fieldName) throws NoSuchFieldException {
+        Class<?> clazz = (object instanceof Class<?>) ? (Class<?>) object : object.getClass();
+
+        while (clazz != Object.class) {
+            try {
+                Field field = clazz.getDeclaredField(fieldName);
+                field.setAccessible(true);
+                return field;
+            } catch (NoSuchFieldException e) {
+                clazz = clazz.getSuperclass();
+            }
+        }
+
+        throw new NoSuchFieldException("Field " + fieldName + " not found in " + object.getClass());
     }
 
     /**
      * 设置对象的属性值
-     * 参数：对象、属性名，属性值
      */
-    public static void setFieldValue(Object object, String fieldName, Object value) {
-        try {
-            Field field = getDeclaredField(object, fieldName);
-            field.setAccessible(true);
-            field.set(object, value);
-        } catch (Exception var4) {
-            throw new RuntimeException("setFieldValue exception, object = " + object + ", fieldName = " + fieldName, var4);
-        }
+    public static void setFieldValue(Object object, String fieldName, Object value) throws Exception {
+        Field field = getDeclaredField(object, fieldName);
+        field.set(object, value);
     }
 
     /**
-     * 获取对象的Object类型属性值
-     * 参数：对象、属性名
+     * 获取对象的属性值
      */
-    public static Object getFieldValue(Object object, String fieldName) {
-        try {
-            Field field = getDeclaredField(object, fieldName);
-            field.setAccessible(true);
-            return field.get(object);
-        } catch (Exception var3) {
-            throw new RuntimeException("getFieldValue exception, object = " + object + ", fieldName = " + fieldName, var3);
-        }
-    }
-
-    public static Class classForName(String className) {
-        try {
-            return Class.forName(className);
-        } catch (ClassNotFoundException var2) {
-            throw new RuntimeException("getClass exception, className = " + className, var2);
-        }
-    }
-
-    public static Object stubAsInterface(String clazz, IBinder binder) {
-        return stubAsInterface(classForName(clazz), binder);
+    public static Object getFieldValue(Object object, String fieldName) throws Exception {
+        Field field = getDeclaredField(object, fieldName);
+        return field.get(object);
     }
 
     /**
-     * 反射调用一个静态类的静态方法，使用示例：
-     * String className = "com.qihoo.gamecenter.sdk.matrix.Matrix";
-     * Class<?>[] parameterTypes = {Activity.class};
-     * Object[] parameters = {PortraitSdkUserActivity.this};
-     * ReflectUtils.invokeStaticMethod(className,"openFloatWindow",parameterTypes,parameters);
+     * 根据类名加载类
      */
-    public static Object invokeStaticMethod(String className, String methodName, Class<?>[] parameterTypes, Object... parameters) {
-        try {
-            Method method = getDeclaredMethod(classForName(className), methodName, parameterTypes);
-            method.setAccessible(true);
-            return method.invoke((Object) null, parameters);
-        } catch (Exception var5) {
-            throw new RuntimeException("invokeStaticMethod exception, className = " + className + ", methodName = " + methodName, var5);
-        }
-    }
-
-    public static Object stubAsInterface(Class clazz, IBinder binder) {
-        try {
-            return clazz.getDeclaredMethod("asInterface", IBinder.class).invoke((Object) null, binder);
-        } catch (IllegalAccessException var3) {
-            throw new RuntimeException(var3);
-        } catch (InvocationTargetException var4) {
-            throw new RuntimeException(var4);
-        } catch (NoSuchMethodException var5) {
-            throw new RuntimeException(var5);
-        }
-    }
-
-    public static Method getMethod(String calssName, String methodName, Class<?>... paremerters) {
-        Method method = null;
-
-        try {
-            Class<?> mClass = Class.forName(calssName);
-            method = mClass.getDeclaredMethod(methodName, paremerters);
-        } catch (SecurityException | NoSuchMethodException | ClassNotFoundException var5) {
-            var5.printStackTrace();
-        }
-
-        return method;
-    }
-
-    public static Method getMethod(Class<?> cls, String methodName, Class<?>[] paramTypes) {
-        Method method = null;
-        if (methodName != null && methodName.length() > 0) {
-            try {
-                method = cls.getMethod(methodName, paramTypes);
-            } catch (NoSuchMethodException var5) {
-                var5.printStackTrace();
-            }
-        }
-
-        return method;
+    public static Class<?> classForName(String className) throws ClassNotFoundException {
+        return Class.forName(className);
     }
 
     /**
-     * 获取当前的Application对象
+     * 获取当前Application上下文（必须在主线程调用）
      */
     public static Context getApplicationContext() {
-        AppLogUtils.d("ReflectUtil", "must be MainThread!" + (Thread.currentThread().getId() != Looper.getMainLooper().getThread().getId()));
-        Context context = null;
-
         try {
-            Class<?> clazz = Class.forName("android.app.ActivityThread");
-            Method method = clazz.getDeclaredMethod("currentApplication");
-            context = (Context) method.invoke((Object) null);
-        } catch (SecurityException | IllegalArgumentException | NoSuchMethodException | InvocationTargetException | IllegalAccessException | ClassNotFoundException var3) {
-            var3.printStackTrace();
-        }
-
-        return context;
-    }
-
-    public static Constructor getObjectConstructor(String className, Class... paramsTypes) {
-        try {
-            return Class.forName(className).getConstructor(paramsTypes);
-        } catch (NoSuchMethodException var3) {
-            throw new RuntimeException(var3);
-        } catch (ClassNotFoundException var4) {
-            throw new RuntimeException(var4);
-        }
-    }
-
-    public static Object getObjectNewInstance(String className, Class[] paramsTypes, Object... args) {
-        try {
-            return Class.forName(className).getConstructor(paramsTypes).newInstance(args);
-        } catch (Exception var4) {
-            throw new RuntimeException(var4);
+            Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+            Method currentApplicationMethod = activityThreadClass.getDeclaredMethod("currentApplication");
+            currentApplicationMethod.setAccessible(true);
+            return (Context) currentApplicationMethod.invoke(null);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to get application context", e);
         }
     }
 
     /**
-     * 获取对象的Object类型属性值
-     * 参数：对象、属性名
+     * 创建指定类的新实例
      */
-    public static Object getField(Object obj, String fieldName) throws NoSuchFieldException, IllegalAccessException {
-        return prepareField(obj.getClass(), fieldName).get(obj);
+    public static <T> T createInstance(Class<T> clazz, Class<?>[] paramTypes, Object... args) throws Exception {
+        Constructor<T> constructor = clazz.getConstructor(paramTypes);
+        return constructor.newInstance(args);
     }
 
     /**
-     * 设置对象的属性值
-     * 参数：对象、属性名，属性值
+     * 获取静态字段的值
      */
-    public static void setField(Object obj, String fieldName, Object value) throws NoSuchFieldException, IllegalAccessException {
-        prepareField(obj.getClass(), fieldName).set(obj, value);
+    public static Object getStaticFieldValue(Class<?> clazz, String fieldName) throws Exception {
+        Field field = getDeclaredField(clazz, fieldName);
+        return field.get(null);
     }
 
     /**
-     * 获取对象的int类型属性值
-     * 参数：对象、属性名
+     * 设置静态字段的值
      */
-    public static int getIntField(Object object, String fieldName) {
-        try {
-            return object.getClass().getDeclaredField(fieldName).getInt(object);
-        } catch (IllegalAccessException var3) {
-            throw new RuntimeException(var3);
-        } catch (NoSuchFieldException var4) {
-            throw new RuntimeException(var4);
-        }
+    public static void setStaticFieldValue(Class<?> clazz, String fieldName, Object value) throws Exception {
+        Field field = getDeclaredField(clazz, fieldName);
+        field.set(null, value);
     }
 
     /**
-     * 获取对象的object类型属性值
-     * 参数：对象、属性名
+     * 通过asInterface方法获取接口实例
      */
-    public static Object getObjectField(Object object, String fieldName) {
+    public static Object stubAsInterface(Class<?> clazz, IBinder binder) {
         try {
-            return object.getClass().getDeclaredField(fieldName).get(object);
-        } catch (IllegalAccessException var3) {
-            throw new RuntimeException(var3);
-        } catch (NoSuchFieldException var4) {
-            throw new RuntimeException(var4);
-        }
-    }
-
-    public static Object getObjectFieldNoDeclared(Object object, String fieldName) {
-        try {
-            return object.getClass().getField(fieldName).get(object);
-        } catch (IllegalAccessException var3) {
-            throw new RuntimeException(var3);
-        } catch (NoSuchFieldException var4) {
-            throw new RuntimeException(var4);
-        }
-    }
-
-    /**
-     * 获取一个静态类的int属性值
-     * 参数：类名(如BaseUtils.class.getName())、属性名
-     */
-    public static int getStaticIntField(String className, String fieldName) {
-        try {
-            Field field = Class.forName(className).getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field.getInt((Object) null);
-        } catch (IllegalAccessException var3) {
-            throw new RuntimeException(var3);
-        } catch (NoSuchFieldException var4) {
-            throw new RuntimeException(var4);
-        } catch (ClassNotFoundException var5) {
-            throw new RuntimeException(var5);
-        }
-    }
-
-    /**
-     * 设置一个静态类的Object属性值
-     * 参数：类名(如BaseUtils.class.getName())、属性名、属性值
-     */
-    public static void setStaticObjectField(String className, String fieldName, Object value) {
-        try {
-            Field field = Class.forName(className).getDeclaredField(fieldName);
-            field.setAccessible(true);
-            field.set((Object) null, value);
-        } catch (IllegalAccessException var4) {
-            throw new RuntimeException(var4);
-        } catch (NoSuchFieldException var5) {
-            throw new RuntimeException(var5);
-        } catch (ClassNotFoundException var6) {
-            throw new RuntimeException(var6);
-        }
-    }
-
-    /**
-     * 获取一个静态类的Object属性值
-     * 参数：类名(如BaseUtils.class.getName())、属性名
-     */
-    public static Object getStaticObjectField(String className, String fieldName) {
-        try {
-            Field field = Class.forName(className).getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field.get((Object) null);
-        } catch (IllegalAccessException var3) {
-            throw new RuntimeException(var3);
-        } catch (NoSuchFieldException var4) {
-            throw new RuntimeException(var4);
-        } catch (ClassNotFoundException var5) {
-            throw new RuntimeException(var5);
-        }
-    }
-
-    public static Field prepareField(Class<?> c, String fieldName) throws NoSuchFieldException {
-        while (true) {
-            if (c != null) {
-                Field var3;
-                try {
-                    Field f = c.getDeclaredField(fieldName);
-                    f.setAccessible(true);
-                    var3 = f;
-                } catch (Exception var7) {
-                    continue;
-                } finally {
-                    c = c.getSuperclass();
-                }
-
-                return var3;
-            }
-
-            throw new NoSuchFieldException();
+            Method asInterfaceMethod = clazz.getDeclaredMethod("asInterface", IBinder.class);
+            asInterfaceMethod.setAccessible(true);
+            return asInterfaceMethod.invoke(null, binder);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to call asInterface method", e);
         }
     }
 }
