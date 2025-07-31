7902da76702734301639331267881be164e4dda1
weichenglin1 <weichenglin1@360.cn>
2025-07-31 18:11:59 +0800

    upgrade
    Former-commit-id: e89d07daa187ff880db32c4bd35b92890d651464

--- app/src/main/java/com/wcl/test/helper/ShowFragmentHelper.java
+++ app/src/main/java/com/wcl/test/helper/ShowFragmentHelper.java
@@ -10,88 +10,103 @@ import androidx.fragment.app.FragmentTransaction;
 import com.wcl.test.base.BaseFragment;
 
 /**
+ * Helper class to manage showing and switching tab fragments.
  * Created by chenglin on 2017-11-28.
  */
-
 public class ShowFragmentHelper {
-    private BaseFragment[] mFragArray;
-    private FragmentManager mFragmentManager;
-    private Class<?>[] FRAGMENTS;
-    public int mSelectedTab;
+    private final BaseFragment[] mFragArray;
+    private final FragmentManager mFragmentManager;
+    private final Class<? extends BaseFragment>[] mFragmentClasses;
+    private int mSelectedTab = -1;
 
     /**
-     * fragmentManager 必须特别注意：
-     * 如果是在Activity里需要传入的是getSupportFragmentManager();
-     * 如果是在嵌套的fragment里面需要调用getChildFragmentManager();
-     * 否则会出问题。
+     * Constructor.
+     *
+     * @param fragmentManager Use getSupportFragmentManager() in Activity,
+     *                        getChildFragmentManager() in Fragment.
+     * @param fragmentClasses Array of fragment classes to manage.
      */
-    public ShowFragmentHelper(FragmentManager fragmentManager, Class<?>[] fragments) {
-        if (fragments == null || fragments.length <= 0) {
-            throw new IllegalArgumentException("FRAGMENTS.length must >0");
+    public ShowFragmentHelper(FragmentManager fragmentManager, Class<? extends BaseFragment>[] fragmentClasses) {
+        if (fragmentClasses == null || fragmentClasses.length == 0) {
+            throw new IllegalArgumentException("Fragment classes must not be null or empty");
         }
 
         this.mFragmentManager = fragmentManager;
-        this.FRAGMENTS = fragments;
-        mFragArray = new BaseFragment[fragments.length];
-    }
+        this.mFragmentClasses = fragmentClasses;
+        this.mFragArray = new BaseFragment[fragmentClasses.length];
 
-    private ShowFragmentHelper() {
+        // Attempt to restore fragments from FragmentManager after configuration change
+        for (int i = 0; i < fragmentClasses.length; i++) {
+            Fragment existing = fragmentManager.findFragmentByTag(fragmentClasses[i].getName());
+            if (existing instanceof BaseFragment) {
+                mFragArray[i] = (BaseFragment) existing;
+            }
+        }
     }
 
     public BaseFragment showTabFragment(@IdRes int viewId, int index) {
         return showTabFragment(viewId, null, index);
     }
 
-    public BaseFragment showTabFragment(@IdRes int viewId, Bundle bundle, int index) {
-        if (FRAGMENTS == null || FRAGMENTS.length <= 0) {
-            throw new IllegalArgumentException("FRAGMENTS.length must >0");
-        }
-        if (index < 0 || index >= FRAGMENTS.length) {
-            throw new IllegalArgumentException("index must >0 And index must < FRAGMENTS.length");
-        }
+    public BaseFragment showTabFragment(@IdRes int viewId, Bundle args, int index) {
+        validateIndex(index);
+
         FragmentTransaction ft = mFragmentManager.beginTransaction();
 
-        if (mFragArray[index] == null) {
-            try {
-                //非常重要，勿改：此处这么写是为了解决当Activity被回收后，重新onCreate()时会创建新的fragment的问题。
-                //具体分析见：https://mp.weixin.qq.com/s/mwTtxk4YfYWCG4m6n_ropw  --by chenglin 2018年7月2日
-                BaseFragment tempFragment = (BaseFragment) mFragmentManager.findFragmentByTag(FRAGMENTS[index].getName());
-                if (tempFragment != null) {
-                    mFragArray[index] = tempFragment;
-                } else {
-                    mFragArray[index] = (BaseFragment) FRAGMENTS[index].newInstance();
-                }
-
-                if (bundle != null) {
-                    mFragArray[index].setArguments(bundle);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        BaseFragment fragment = mFragArray[index];
+        if (fragment == null) {
+            fragment = createFragment(index, args);
+            mFragArray[index] = fragment;
+            addFragment(viewId, fragment, ft);
+        } else if (args != null && fragment.getArguments() == null) {
+            fragment.setArguments(args);
         }
 
-        addFragment(viewId, mFragArray[index], ft);
-        showFragment(ft, index);
+        showOnlyFragment(ft, index);
         ft.commitAllowingStateLoss();
+
         mSelectedTab = index;
-        return mFragArray[index];
+        return fragment;
     }
 
-    private void showFragment(FragmentTransaction ft, final int index) {
-        for (int i = 0; i < mFragArray.length; i++) {
-            if (mFragArray[i] != null) {
-                if (i == index) {
-                    ft.show(mFragArray[i]);
-                } else {
-                    ft.hide(mFragArray[i]);
-                }
+    private void validateIndex(int index) {
+        if (index < 0 || index >= mFragmentClasses.length) {
+            throw new IndexOutOfBoundsException("Index " + index + " out of bounds for fragment array");
+        }
+    }
+
+    private BaseFragment createFragment(int index, Bundle args) {
+        try {
+            BaseFragment fragment = mFragmentClasses[index].newInstance();
+            if (args != null) {
+                fragment.setArguments(args);
             }
+            return fragment;
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to instantiate fragment: " + mFragmentClasses[index].getName(), e);
         }
     }
 
     private void addFragment(@IdRes int viewId, Fragment fragment, FragmentTransaction ft) {
-        if (!fragment.isAdded() && fragment.getTag() == null) {
+        if (!fragment.isAdded()) {
             ft.add(viewId, fragment, fragment.getClass().getName());
         }
+    }
+
+    private void showOnlyFragment(FragmentTransaction ft, int showIndex) {
+        for (int i = 0; i < mFragArray.length; i++) {
+            Fragment frag = mFragArray[i];
+            if (frag != null) {
+                if (i == showIndex) {
+                    ft.show(frag);
+                } else {
+                    ft.hide(frag);
+                }
+            }
+        }
+    }
+
+    public int getSelectedTab() {
+        return mSelectedTab;
     }
 }
