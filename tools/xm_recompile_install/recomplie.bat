fa3ca86c6be8f30c8185612ad41095ecc222ca70
weichenglin1 <weichenglin1@360.cn>
2025-08-11 15:40:21 +0800

    upgrade
    Former-commit-id: 0683daa875fc6d7636e968269c6017fad1d0a799

--- tools/xm_recompile_install/recomplie.bat
+++ tools/xm_recompile_install/recomplie.bat
@@ -23,7 +23,10 @@ cd /d %INIT_DIR%
 set RELEASE_APK=%SRC_DIR%\plugins\%PLUGIN_LIBRARY%\app\build\outputs\apk\release
 del "%RELEASE_APK%\%PLUGIN_JAR%" >nul 2>&1
 ren "%RELEASE_APK%\*.apk" %PLUGIN_JAR%
-copy /Y "%RELEASE_APK%\%PLUGIN_JAR%" .
+if not exist assets\plugins (
+    mkdir assets\plugins
+)
+copy /Y "%RELEASE_APK%\%PLUGIN_JAR%" assets\plugins\
 
 :: 配置所需工具
 set ZIPALIGN=tools\zipalign.exe
@@ -36,7 +39,7 @@ copy /Y %APK_NAME% %OUT_APK%
 
 :: 替换 APK 中的 assets/plugins/.jar 文件
 echo 2.Updating %ASSET_PATH% in APK...
-%SEVEN_ZIP% u -tzip %OUT_APK% %ASSET_PATH% -spf2 -ssc -up0q3
+%SEVEN_ZIP% a -tzip "%OUT_APK%" "%ASSET_PATH%" -spf2 -ssc
 
 :: 对齐 APK
 echo 3.Aligning APK...
@@ -59,6 +62,5 @@ del unsign.apk signed.apk.idsig modified_aligned_signed.apk aligned.apk >nul 2>&
 :: 安装 APK
 echo 5.Installing APK...
 %ADB% install -r signed.apk
-
 
 pause
