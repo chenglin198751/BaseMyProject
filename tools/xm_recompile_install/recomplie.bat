467b27e03fcf3ebc578008d16252ad55a9e0d7c5
weichenglin1 <weichenglin1@360.cn>
2025-08-14 17:40:13 +0800

    xx
    Former-commit-id: 4b471f8610018ab4c4aea364b8543ca1bd4674fd

--- tools/xm_recompile_install/recomplie.bat
+++ tools/xm_recompile_install/recomplie.bat
@@ -2,23 +2,29 @@
 setlocal
 
 :: 配置变量
-set SRC_DIR=D:\AndroidCode\zhushou
+set SRC_DIR=D:\AndroidCode\develop_xmkw
 set APK_NAME=host.apk
-set PLUGIN_JAR=com.qihoo.plugin.modulation.jar
-set PLUGIN_LIBRARY=modulation
+
+:: 主页列表
+:: set PLUGIN_JAR=com.qihoo.plugin.modulation.jar
+:: set PLUGIN_LIBRARY=modulation
+
+:: 软件详情页
+set PLUGIN_JAR=recommend.jar
+set PLUGIN_LIBRARY=app_info
 
 set INIT_DIR=%cd%
 set ASSET_PATH=assets/plugins/%PLUGIN_JAR%
 set OUT_APK=modified_aligned_signed.apk
-set KEYSTORE=tools\keystore_debug.jks
-set ALIAS=young_debug
-set STOREPASS=123abc
-set KEYPASS=123abc
+set KEYSTORE=tools\debug.keystore
+set ALIAS=androiddebugkey
+set STOREPASS=android
+set KEYPASS=android
 
 :: 使用gradlew编译插件apk，并把apk重命名为jar
-echo 1.start build %PLUGIN_JAR%
-cd /d %SRC_DIR%
-call gradlew :plugins:%PLUGIN_LIBRARY%:app:assembleRelease --stacktrace --no-daemon || (echo gradle build failed & exit /b 1)
+::echo 1.start build %PLUGIN_JAR%
+::cd /d %SRC_DIR%
+::call gradlew :plugins:%PLUGIN_LIBRARY%:app:assembleRelease --stacktrace --no-daemon || (echo gradle build failed & exit /b 1)
 cd /d %INIT_DIR%
 set RELEASE_APK=%SRC_DIR%\plugins\%PLUGIN_LIBRARY%\app\build\outputs\apk\release
 del "%RELEASE_APK%\%PLUGIN_JAR%" >nul 2>&1
