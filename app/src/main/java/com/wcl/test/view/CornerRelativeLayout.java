c9c7cca515856dc32da8e464fa8e3bf1838cebb3
weichenglin1 <weichenglin1@360.cn>
2025-09-02 13:49:41 +0800

    upgrade
    Former-commit-id: 43487c42b52fd946a8d5438861b2d994884693a6

--- app/src/main/java/com/wcl/test/view/CornerRelativeLayout.java
+++ app/src/main/java/com/wcl/test/view/CornerRelativeLayout.java
@@ -2,66 +2,74 @@ package com.wcl.test.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Path;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
 import com.wcl.test.utils.AppBaseUtils;
 
+/**
+ * Created by chenglin on 2017-6-21.
+ */
+
 public class CornerRelativeLayout extends RelativeLayout {
-    private float topLeftRadius;
-    private float topRightRadius;
-    private float bottomLeftRadius;
-    private float bottomRightRadius;
-    private Path clipPath;
+    public CornerRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
 
     public CornerRelativeLayout(Context context) {
         super(context);
         init();
     }
 
-    public CornerRelativeLayout(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
-
-    public CornerRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-    }
+    private final RectF roundRect = new RectF();
+    private int corner = 4;
+    private final Paint maskPaint = new Paint();
+    private final Paint zonePaint = new Paint();
 
     private void init() {
-        clipPath = new Path();
+        maskPaint.setAntiAlias(true);
+        maskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+
+        zonePaint.setAntiAlias(true);
+        zonePaint.setColor(Color.WHITE);
+
+        if (getTag() != null && getTag() instanceof String tag) {
+            if (TextUtils.isDigitsOnly(tag.trim())) {
+                corner = Integer.parseInt(tag.trim());
+            }
+        }
+        corner = AppBaseUtils.dip2px(corner);
     }
 
-    public void setCorner(int radii) {
-        setCorner(radii, radii, radii, radii);
-    }
-
-    public void setCorner(float topLeft, float topRight, float bottomLeft, float bottomRight) {
-        this.topLeftRadius = AppBaseUtils.dip2px(topLeft);
-        this.topRightRadius = AppBaseUtils.dip2px(topRight);
-        this.bottomLeftRadius = AppBaseUtils.dip2px(bottomLeft);
-        this.bottomRightRadius = AppBaseUtils.dip2px(bottomRight);
+    /**
+     * 设置View 的显示角度
+     *
+     * @param corner 单位是像素
+     */
+    public void setCorner(int corner) {
+        this.corner = corner;
         invalidate();
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        clipPath.reset();
-        RectF rect = new RectF(0, 0, w, h);
-        float[] radii = {topLeftRadius, topLeftRadius, topRightRadius, topRightRadius,
-                bottomRightRadius, bottomRightRadius, bottomLeftRadius, bottomLeftRadius};
-        clipPath.addRoundRect(rect, radii, Path.Direction.CW);
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+        roundRect.set(0, 0, getWidth(), getHeight());
     }
 
     @Override
-    protected void dispatchDraw(Canvas canvas) {
-        int save = canvas.save();
-        canvas.clipPath(clipPath);
-        super.dispatchDraw(canvas);
-        canvas.restoreToCount(save);
+    public void draw(Canvas canvas) {
+        canvas.saveLayer(roundRect, zonePaint, Canvas.ALL_SAVE_FLAG);
+        canvas.drawRoundRect(roundRect, corner, corner, zonePaint);
+        canvas.saveLayer(roundRect, maskPaint, Canvas.ALL_SAVE_FLAG);
+        super.draw(canvas);
+        canvas.restore();
     }
 }
