1f1682b134e520e6a9a3665393921f7b9d577ccf
weichenglin1 <weichenglin1@360.cn>
2025-10-30 20:27:39 +0800

    upgrade
    Former-commit-id: c7f8ecd7056bf24a63037a14c53f1c9b1fa1d55c

--- app/src/main/java/com/wcl/test/test/TestRecyclerViewRefreshActivity.java
+++ app/src/main/java/com/wcl/test/test/TestRecyclerViewRefreshActivity.java
@@ -19,6 +19,7 @@ import com.wcl.test.R;
 import com.wcl.test.base.BaseActivity;
 import com.wcl.test.base.BaseRecyclerViewAdapter;
 import com.wcl.test.listener.OnSingleClickListener;
+import com.wcl.test.utils.CountDownManager;
 import com.wcl.test.utils.SmartImageLoader;
 import com.wcl.test.view.pullrefresh.PullToRefreshView;
 
@@ -124,78 +125,6 @@ public class TestRecyclerViewRefreshActivity extends BaseActivity {
         super.onStop();
         CountDownManager.getInstance().removeListener(mAdapter);
         CountDownManager.getInstance().stop();
-    }
-
-    // --------------------------
-    // 倒计时管理器（全局单例）
-    // --------------------------
-    public static class CountDownManager {
-        private static final CountDownManager INSTANCE = new CountDownManager();
-        private final List<WeakReference<TickListener>> listeners = new ArrayList<>();
-        private final Handler handler = new Handler(Looper.getMainLooper());
-        private boolean isRunning = false;
-
-        public interface TickListener {
-            void onTick();
-        }
-
-        public static CountDownManager getInstance() {
-            return INSTANCE;
-        }
-
-        public void addListener(TickListener listener) {
-            synchronized (listeners) {
-                listeners.add(new WeakReference<>(listener));
-            }
-        }
-
-        public void removeListener(TickListener listener) {
-            synchronized (listeners) {
-                Iterator<WeakReference<TickListener>> it = listeners.iterator();
-                while (it.hasNext()) {
-                    TickListener l = it.next().get();
-                    if (l == null || l == listener) {
-                        it.remove();
-                    }
-                }
-            }
-        }
-
-        private final Runnable tickRunnable = new Runnable() {
-            @Override
-            public void run() {
-                notifyAllListeners();
-                if (isRunning) {
-                    handler.postDelayed(this, 1000);
-                }
-            }
-        };
-
-        private void notifyAllListeners() {
-            synchronized (listeners) {
-                Iterator<WeakReference<TickListener>> it = listeners.iterator();
-                while (it.hasNext()) {
-                    TickListener l = it.next().get();
-                    if (l != null) {
-                        l.onTick();
-                    } else {
-                        it.remove();
-                    }
-                }
-            }
-        }
-
-        public void start() {
-            if (!isRunning) {
-                isRunning = true;
-                handler.post(tickRunnable);
-            }
-        }
-
-        public void stop() {
-            isRunning = false;
-            handler.removeCallbacks(tickRunnable);
-        }
     }
 
     // --------------------------
---
+++ app/src/main/java/com/wcl/test/utils/CountDownManager.java
@@ -0,0 +1,78 @@
+package com.wcl.test.utils;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class CountDownManager {
+        private static final CountDownManager INSTANCE = new CountDownManager();
+        private final List<WeakReference<TickListener>> listeners = new ArrayList<>();
+        private final Handler handler = new Handler(Looper.getMainLooper());
+        private boolean isRunning = false;
+
+        public interface TickListener {
+            void onTick();
+        }
+
+        public static CountDownManager getInstance() {
+            return INSTANCE;
+        }
+
+        public void addListener(TickListener listener) {
+            synchronized (listeners) {
+                listeners.add(new WeakReference<>(listener));
+            }
+        }
+
+        public void removeListener(TickListener listener) {
+            synchronized (listeners) {
+                Iterator<WeakReference<TickListener>> it = listeners.iterator();
+                while (it.hasNext()) {
+                    TickListener l = it.next().get();
+                    if (l == null || l == listener) {
+                        it.remove();
+                    }
+                }
+            }
+        }
+
+        private final Runnable tickRunnable = new Runnable() {
+            @Override
+            public void run() {
+                notifyAllListeners();
+                if (isRunning) {
+                    handler.postDelayed(this, 1000);
+                }
+            }
+        };
+
+        private void notifyAllListeners() {
+            synchronized (listeners) {
+                Iterator<WeakReference<TickListener>> it = listeners.iterator();
+                while (it.hasNext()) {
+                    TickListener l = it.next().get();
+                    if (l != null) {
+                        l.onTick();
+                    } else {
+                        it.remove();
+                    }
+                }
+            }
+        }
+
+        public void start() {
+            if (!isRunning) {
+                isRunning = true;
+                handler.post(tickRunnable);
+            }
+        }
+
+        public void stop() {
+            isRunning = false;
+            handler.removeCallbacks(tickRunnable);
+        }
+    }
