781df065fc055a547d8158ef67ddaa0d0cbb84c3
weichenglin1 <weichenglin1@360.cn>
2026-01-20 12:01:12 +0800

    added onSelected()
    Former-commit-id: 5aeb7f428ff4e05315bbd7765a41222845198ca6

--- app/src/main/java/com/wcl/test/helper/ShowFragmentHelper.java
+++ app/src/main/java/com/wcl/test/helper/ShowFragmentHelper.java
@@ -63,6 +63,10 @@ public class ShowFragmentHelper {
         showOnlyFragment(ft, index);
         ft.commitAllowingStateLoss();
 
+        if (mSelectedIndex != index) {
+            fragment.onSelected();
+        }
+
         mSelectedIndex = index;
         return fragment;
     }
