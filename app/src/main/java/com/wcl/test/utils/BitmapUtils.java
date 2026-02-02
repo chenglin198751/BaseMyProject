751c5eefdff2cf4e0d2d1a837dc99f5159798ecc
weichenglin1 <weichenglin1@360.cn>
2026-02-02 11:41:42 +0800

    chatGPT实现多任务断点续传下载管理
    Former-commit-id: 707886cd8d6ee78714a692f3316510e06547c6f2

--- app/src/main/java/com/wcl/test/utils/BitmapUtils.java
+++ app/src/main/java/com/wcl/test/utils/BitmapUtils.java
@@ -143,7 +143,7 @@ public class BitmapUtils {
         }
 
         new Thread(() -> {
-            File dir = new File(FileUtils.getAppFilesPath());
+            File dir = new File(AppFileUtils.getAppFilesPath());
             if (!dir.exists() && !dir.mkdirs()) {
                 postToUi(() -> callback.onFailed("Create directory failed"));
                 return;
