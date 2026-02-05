a94f86beb44725da49dcc5df5ded245aa53ae8d3
weichenglin1 <weichenglin1@360.cn>
2026-02-05 18:25:27 +0800

    Revert "抛弃SP，使用MMKV"
    This reverts commit 8f848a8f24ca239cc01205697481bccaa7308d43 [formerly 9cce7534a4cc352bd99423637b91fe319ff5be69].
    
    
    Former-commit-id: 9034b3da6b441b8d2840ce02e91b8cf82a4df2ec

--- app/src/main/java/com/wcl/test/storage/PreferAppSettings.java
+++ app/src/main/java/com/wcl/test/storage/PreferAppSettings.java
@@ -4,13 +4,14 @@ import com.tencent.mmkv.MMKV;
 
 public class PreferAppSettings {
 
+    private static final MMKV kv = MMKV.defaultMMKV();
     private static final String update_dialog_times = "update_dialog_times";
 
     public static Long getUpdateDialogTimes() {
-        return MMKV.defaultMMKV().decodeLong(update_dialog_times, 0);
+        return kv.decodeLong(update_dialog_times, 0);
     }
 
     public static void setUpdateDialogTimes(long value) {
-        MMKV.defaultMMKV().encode(update_dialog_times, value);
+        kv.encode(update_dialog_times, value);
     }
 }
--- app/src/main/java/com/wcl/test/storage/ToggleSettings.java
+++ app/src/main/java/com/wcl/test/storage/ToggleSettings.java
@@ -4,22 +4,23 @@ import com.tencent.mmkv.MMKV;
 
 public class ToggleSettings {
 
+    private static final MMKV kv = MMKV.defaultMMKV();
     private static final String KEY_LOG_TOGGLE = "KEY_LOG_TOGGLE";
     private static final String KEY_DEBUG_TOGGLE = "KEY_DEBUG_TOGGLE";
 
     public static void setLogEnable(boolean value) {
-        MMKV.defaultMMKV().encode(KEY_LOG_TOGGLE, value);
+        kv.encode(KEY_LOG_TOGGLE, value);
     }
 
     public static boolean getLogEnable() {
-        return MMKV.defaultMMKV().decodeBool(KEY_LOG_TOGGLE, false);
+        return kv.decodeBool(KEY_LOG_TOGGLE, false);
     }
 
     public static void setDebugEnable(boolean value) {
-        MMKV.defaultMMKV().encode(KEY_DEBUG_TOGGLE, value);
+        kv.encode(KEY_DEBUG_TOGGLE, value);
     }
 
     public static boolean getDebugEnable() {
-        return MMKV.defaultMMKV().decodeBool(KEY_DEBUG_TOGGLE, false);
+        return kv.decodeBool(KEY_DEBUG_TOGGLE, false);
     }
 }
