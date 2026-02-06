b2034311745bd21d56b00b3caf9a49501c59c43b
weichenglin1 <weichenglin1@360.cn>
2026-02-06 17:35:46 +0800

    Codex优化代码
    Former-commit-id: 362b1611f10cc1e38c026051e39786609524be9c

---
+++ app/src/main/java/com/wcl/test/view/image/IRoundedMethod.java
@@ -0,0 +1,15 @@
+package com.wcl.test.view.image;
+
+interface IRoundedMethod {
+    void setCornerRadius(float radius);
+
+    void setOval(boolean oval);
+
+    void setAspectRatio(float ratio);
+
+    void setBorderWidth(float width);
+
+    void setBorderColor(int color);
+
+    void setSolidColor(int color);
+}
