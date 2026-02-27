0c398091cf4c705ed985b40f3b6937d82364d059
weichenglin1 <weichenglin1@360.cn>
2026-02-27 11:25:56 +0800

    upgrade
    Former-commit-id: f562d43bd59108cbf09e11b08f9d8c4e88dd635d

--- app/src/main/java/com/wcl/test/main/MainFirstFragment.java
+++ app/src/main/java/com/wcl/test/main/MainFirstFragment.java
@@ -8,9 +8,6 @@ import android.view.ViewGroup;
 import com.wcl.test.R;
 import com.wcl.test.base.BaseFragment;
 import com.wcl.test.databinding.MainFirstFragLayoutBinding;
-import com.wcl.test.download.DownloadCallback2;
-import com.wcl.test.download.DownloadManager;
-import com.wcl.test.download.DownloadTask;
 
 
 /**
@@ -29,26 +26,6 @@ public class MainFirstFragment extends BaseFragment {
         mViewBinding = MainFirstFragLayoutBinding.bind(((ViewGroup) view).getChildAt(0));
 
         mViewBinding.viewLeft.setOnClickListener(v -> {
-            String url ="https://www.wanandroid.com/banner/json";
-            HttpUtils.post(getContext(), url, null, new HttpUtils.HttpCallback() {
-                @Override
-                public void onResult(boolean success, String result) {
-                    Log.v("HttpUtils","success:"+success + ",result:"+result);
-                }
-            });
-
-            String url2 = "http://qd.shouji.qihucdn.com/media/949541c42745397670cd5935dd89835b/674fba4e6f5e6.zip";
-            HttpUtils.download(url2, new HttpUtils.DownloadCallback() {
-                @Override
-                public void onProgress(long total, long current, float percent) {
-                    Log.v("tag_99","percent="+percent);
-                }
-
-                @Override
-                public void onFinished(boolean success, String filePath, String error) {
-                    Log.v("tag_99","success:"+success + ",filePath:"+filePath + ",error:"+error);
-                }
-            });
         });
 
         String url = "http://qd.shouji.qihucdn.com/media/d22eee36c269dcae8dbfc6a469d02ffc/6602326c507c2.png";
