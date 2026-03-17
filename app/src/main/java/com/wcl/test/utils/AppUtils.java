7138665e991876489d633a9f508c38fd04a17259
weichenglin1 <weichenglin1@360.cn>
2026-03-17 11:52:31 +0800

    upgrade
    Former-commit-id: 348756a64b4cf995869d39fd09a10ba69e7db424

--- app/src/main/java/com/wcl/test/utils/AppUtils.java
+++ app/src/main/java/com/wcl/test/utils/AppUtils.java
@@ -49,15 +49,18 @@ public class AppUtils {
         }
 
         Network network = connectivityManager.getActiveNetwork();
-        if (network != null) {
-            NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
-            if (capabilities != null) {
-                return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
-                        || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
-                        || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
-            }
+        if (network == null) {
+            return false;
         }
-        return true;
+
+        NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
+        if (capabilities == null) {
+            return false;
+        }
+
+        return capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+                || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+                || capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET);
     }
 
     public static String getString(int id) {
