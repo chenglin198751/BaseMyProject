3f828cb6616a7e7fef52aa307f4d1b159759e36d
weichenglin1 <weichenglin1@360.cn>
2026-03-25 10:18:47 +0800

    upgrade
    Former-commit-id: 706fd18c48c3772324f118d0316b6304d33c18ab

--- app/src/main/java/com/wcl/test/view/image/GlideBgImageView.java
+++ app/src/main/java/com/wcl/test/view/image/GlideBgImageView.java
@@ -1,7 +1,6 @@
 package com.wcl.test.view.image;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
 
@@ -53,8 +52,7 @@ public class GlideBgImageView extends RoundedBgImageView {
 
 
     public void loadImage(Object uri, RequestOptions options) {
-//        ColorDrawable d = new ColorDrawable(0x99e8e8e8);
-        ColorDrawable d = new ColorDrawable(Color.BLUE);
+        ColorDrawable d = new ColorDrawable(0x99e8e8e8);
         GlideApp.with(this)
                 .load(uri)
                 .apply(options)
