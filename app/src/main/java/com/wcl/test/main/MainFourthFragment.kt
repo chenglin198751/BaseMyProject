501dad4b3fb3e227cc08a72d770db2b2184e54db
weichenglin1 <weichenglin1@360.cn>
2026-05-22 17:58:47 +0800

    upgrade
    Former-commit-id: 734d1c38e524622f974d10a1db23e60589318716

--- app/src/main/java/com/wcl/test/main/MainFourthFragment.kt
+++ app/src/main/java/com/wcl/test/main/MainFourthFragment.kt
@@ -1,26 +1,35 @@
 package com.wcl.test.main
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import com.wcl.test.base.BaseFragment
-import com.wcl.test.base.viewBinding
 import com.wcl.test.databinding.MainFourthFragLayoutBinding
 
 /**
  * Created by chenglin on 2017-9-14.
  */
 class MainFourthFragment : BaseFragment() {
-    private val binding by viewBinding(MainFourthFragLayoutBinding::inflate)
+    private var _binding: MainFourthFragLayoutBinding? = null
+    private val binding get() = _binding!!
 
     override fun getContentLayout(): Int {
         return 0
     }
 
-    override fun getContentView(): View {
-        return binding.root
+    override fun getContentView(): View? {
+        if (_binding == null) {
+            _binding = MainFourthFragLayoutBinding.inflate(LayoutInflater.from(context))
+        }
+        return _binding?.root
     }
 
-    override fun onViewCreated(savedInstanceState: Bundle?, view: View?) {
-        // 直接使用 binding 即可
+    override fun onViewCreated(savedInstanceState: Bundle?, view: View) {
+        // 使用 binding 进行 UI 操作
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
