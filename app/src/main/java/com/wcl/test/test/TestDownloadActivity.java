9d0081c5e048853a7617c017237c2afd25c801f5
weichenglin1 <weichenglin1@360.cn>
2026-05-28 19:29:06 +0800

    upgrade
    Former-commit-id: 8c9578f529d5a3f8bd493301e51800015d90bc8f

--- app/src/main/java/com/wcl/test/test/TestDownloadActivity.java
+++ app/src/main/java/com/wcl/test/test/TestDownloadActivity.java
@@ -1,12 +1,10 @@
 package com.wcl.test.test;
 
-import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -19,7 +17,6 @@ import com.wcl.test.base.BaseRecyclerViewHolder;
 import com.wcl.test.download.ui.DownloadButton;
 import com.wcl.test.utils.AppUtils;
 import com.wcl.test.view.RecyclerDivider;
-import com.wcl.test.view.image.GlideImageView;
 
 public class TestDownloadActivity extends BaseActivity {
     private TestRecyclerAdapter adapter;
@@ -36,41 +33,41 @@ public class TestDownloadActivity extends BaseActivity {
         adapter = new TestRecyclerAdapter(getContext());
         adapter.setDataList(TestUrls.DownUrls);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+//        recyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
         recyclerView.setAdapter(adapter);
 
-        recyclerView.addItemDecoration(new RecyclerDivider(Color.BLUE, AppUtils.dp2px(20), AppUtils.dp2px(5), true));
+        recyclerView.addItemDecoration(new RecyclerDivider(Color.BLUE, AppUtils.dp2px(5), false));
     }
 
 
     public static class TestRecyclerAdapter extends BaseRecyclerViewAdapter<String> {
+        private final BaseActivity activity;
 
-        private final Context mContext;
-
-        public TestRecyclerAdapter(Context context) {
-            this.mContext = context;
+        public TestRecyclerAdapter(BaseActivity activity) {
+            this.activity = activity;
         }
 
 
         @NonNull
         @Override
         public BaseRecyclerViewHolder<String> onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(mContext).inflate(R.layout.test_download_item, parent, false);
-            return new ListHolder(view);
+            View view = LayoutInflater.from(activity).inflate(R.layout.test_download_item, parent, false);
+            return new ListHolder(activity, view);
         }
 
         private static class ListHolder extends BaseRecyclerViewHolder<String> {
+            private final BaseActivity activity;
             DownloadButton downloadButton;
-            TextView title;
 
-            public ListHolder(View itemView) {
+            public ListHolder(BaseActivity activity, View itemView) {
                 super(itemView);
+                this.activity = activity;
                 downloadButton = itemView.findViewById(R.id.down_btn);
-                title = itemView.findViewById(R.id.title);
             }
 
             @Override
             public void onBind(@NonNull String url, int position) {
-
+                downloadButton.bind(url, activity);
             }
 
         }
