ed6e2d9679b90ec68bb6830e0a00c4aced1c80a1
weichenglin1 <weichenglin1@360.cn>
2026-05-28 19:27:21 +0800

    upgrade
    Former-commit-id: eade89ad218f96c5ebaba953a62dd31c7ed33eec

--- app/src/main/java/com/wcl/test/test/TestUrls.java
+++ app/src/main/java/com/wcl/test/test/TestUrls.java
@@ -45,26 +45,26 @@ public class TestUrls {
     }
 
     static {
-        DownUrls.add("//qd.shouji.qihucdn.com/media/b14f3e91f74b10dd85129d9007c9e8da/6a17b91c66ecf.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/dfad30e3a71e3a7a7fd69ba6a6846ac0/6a17b0830fe55.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/4b100b0ca41e1de89643a482555e187d/6a10269d0b31a.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/b5bbfee33c79320a3e03cea42da12bf1/6a0e761791bb5.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/dc07d99a04bd38e8b85d94cfa9e9b8ca/6a0e4c11ad769.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/e59519558ca94d7b309563bc8dbe95f1/6a06919703448.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/5223f2296e33cd9432c47db306955fab/6a028d8376f8a.apk");
-        DownUrls.add("//d02.qd.shouji.360tpcdn.com/media/e81695553097802369ee9b29b35663dc/6a01b29bcd3b8.mp4");
-        DownUrls.add("//d02.qd.shouji.360tpcdn.com/media/c0399c7bbe85a03938d0c47a7fa8d474/6a01a5eb97373.mp4");
-        DownUrls.add("//d02.qd.shouji.360tpcdn.com/media/5a7efac4f9092c9974af19e9643e304d/6a0193e1b1b5b.mp4");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/435a2edc775a43da7c7eb637be71d64a/69fef04b9f7de.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/d00e9834e1b90c17cdc39c5aec5c99b6/69fdc34b1abe7.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/9edd6de362efcbc7969be19928db38d7/69f05c0e10fda.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/965f49bdf3a62842a205dff4787f588a/69f0145ce2963.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/5f53ba6913f00145a1f396038b9c0b25/69ef3e7ca2eac.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/757af585402f2dd7500e8b22fb083c7c/69e98c4271cae.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/0b68813587bb85709966db66e9c9c397/69e97d763799f.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/cde0697c26b4234627d64ecc42f7a12f/69e961fdc3312.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/e220eb3da87517c7d3337943a06cdca8/69e95435ca4c7.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/4922d2fdcc71fb79a8c7f7d0ac4e6367/69deffa22fa88.apk");
-        DownUrls.add("//qd.shouji.qihucdn.com/media/3a2e4bb0ef1bf4bf635e0f685fddfadd/69dc8fa997454.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/b14f3e91f74b10dd85129d9007c9e8da/6a17b91c66ecf.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/dfad30e3a71e3a7a7fd69ba6a6846ac0/6a17b0830fe55.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/4b100b0ca41e1de89643a482555e187d/6a10269d0b31a.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/b5bbfee33c79320a3e03cea42da12bf1/6a0e761791bb5.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/dc07d99a04bd38e8b85d94cfa9e9b8ca/6a0e4c11ad769.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/e59519558ca94d7b309563bc8dbe95f1/6a06919703448.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/5223f2296e33cd9432c47db306955fab/6a028d8376f8a.apk");
+        DownUrls.add("https://d02.qd.shouji.360tpcdn.com/media/e81695553097802369ee9b29b35663dc/6a01b29bcd3b8.mp4");
+        DownUrls.add("https://d02.qd.shouji.360tpcdn.com/media/c0399c7bbe85a03938d0c47a7fa8d474/6a01a5eb97373.mp4");
+        DownUrls.add("https://d02.qd.shouji.360tpcdn.com/media/5a7efac4f9092c9974af19e9643e304d/6a0193e1b1b5b.mp4");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/435a2edc775a43da7c7eb637be71d64a/69fef04b9f7de.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/d00e9834e1b90c17cdc39c5aec5c99b6/69fdc34b1abe7.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/9edd6de362efcbc7969be19928db38d7/69f05c0e10fda.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/965f49bdf3a62842a205dff4787f588a/69f0145ce2963.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/5f53ba6913f00145a1f396038b9c0b25/69ef3e7ca2eac.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/757af585402f2dd7500e8b22fb083c7c/69e98c4271cae.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/0b68813587bb85709966db66e9c9c397/69e97d763799f.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/cde0697c26b4234627d64ecc42f7a12f/69e961fdc3312.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/e220eb3da87517c7d3337943a06cdca8/69e95435ca4c7.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/4922d2fdcc71fb79a8c7f7d0ac4e6367/69deffa22fa88.apk");
+        DownUrls.add("https://qd.shouji.qihucdn.com/media/3a2e4bb0ef1bf4bf635e0f685fddfadd/69dc8fa997454.apk");
     }
 }
