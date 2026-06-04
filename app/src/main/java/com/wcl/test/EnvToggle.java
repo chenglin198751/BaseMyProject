e7919cfe3d8fdc1a1c4caf3eedf518b153d5f583
weichenglin1 <weichenglin1@360.cn>
2026-06-04 21:00:12 +0800

    upgrade
    Former-commit-id: c84e98283a894d16599196825f68abd9aab3dd36

--- app/src/main/java/com/wcl/test/EnvToggle.java
+++ app/src/main/java/com/wcl/test/EnvToggle.java
@@ -5,7 +5,7 @@ import com.wcl.test.storage.ToggleSettings;
 
 public class EnvToggle {
     private static final boolean isDebug = BuildConfig.DEBUG || ToggleSettings.getDebugEnable();
-    private static final boolean isLog = BuildConfig.LOG_ENABLED || ToggleSettings.getLogEnable();
+    private static final boolean isLog = BuildConfig.LOG || ToggleSettings.getLogEnable();
 
     public static boolean isLog() {
         return isLog;
