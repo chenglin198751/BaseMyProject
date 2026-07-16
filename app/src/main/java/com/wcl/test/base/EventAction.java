3f203e21fe9e310de4579fed232cf32ccef39209
weichenglin1 <weichenglin1@360.cn>
2026-07-16 10:25:38 +0800

    upgrade
    Former-commit-id: 1da839517945984964d1bc5f54fec18da3d6c9a0

--- app/src/main/java/com/wcl/test/base/EventAction.java
+++ app/src/main/java/com/wcl/test/base/EventAction.java
@@ -7,6 +7,7 @@ public class EventAction {
          * 根据开关onKeepSingleActivity()：当前Activity无论打开多少，只保留最后打开的一个
          */
         String ACTION_KEEP_SINGLE_ACTIVITY = "ACTION_SYS_KEEP_SINGLE_ACTIVITY";
+        
         /**
          * 关闭别的Activity，只保留MainActivity不关闭
          */
