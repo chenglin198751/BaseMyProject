559825724998e8374874cd2e4fe237247b4cb0ea
weichenglin1 <weichenglin1@360.cn>
2026-07-30 14:39:58 +0800

    升级 Material 3

--- app/src/main/java/com/wcl/test/main/MainFirstFragment.java
+++ app/src/main/java/com/wcl/test/main/MainFirstFragment.java
@@ -37,9 +37,6 @@ public class MainFirstFragment extends BaseFragment {
 
         String url = "http://qd.shouji.qihucdn.com/media/d22eee36c269dcae8dbfc6a469d02ffc/6602326c507c2.png";
         mBinding.image2.loadImage(url);
-
-//        showWaitDialog();
-        showLoading();
     }
 
     @Override
